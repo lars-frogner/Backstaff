59d58adc412c27c68ba68d8a9e0b29a024935e3a
Lars Frogner <lars_frogner@hotmail.com>
2019-08-30 23:34:10 +0200

    Use functions for tracing instead of struct

--- src/tracing.rs
+++ src/tracing.rs
@@ -2,9 +2,10 @@
 
 pub mod seeding;
 pub mod stepping;
+pub mod field_line;
 
 use num;
-use crate::geometry::{Point3};
+use crate::geometry::Point3;
 use crate::grid::Grid3;
 use crate::field::VectorField3;
 use crate::interpolation::Interpolator3;
@@ -14,34 +15,36 @@ use self::stepping::{Stepper3, StepperResult, StepperInstruction};
 #[allow(non_camel_case_types)]
 pub type ftr = f64;
 
-pub struct Tracer3;
+/// Traces a field line through the given vector field from the given start position,
+/// using the given interpolator and stepper. The given callback closure is called with
+/// the natural position of each step.
+pub fn trace<F, G, I, S, C>(field: &VectorField3<F, G>, interpolator: &I, mut stepper: S, start_position: &Point3<ftr>, callback: &mut C)
+where F: num::Float + std::fmt::Display,
+        G: Grid3<F> + Clone,
+        I: Interpolator3,
+        S: Stepper3,
+        C: FnMut(&Point3<ftr>) -> StepperInstruction
+{
+    match stepper.place(field, interpolator, start_position, callback) {
+        StepperResult::Ok(_) => {},
+        StepperResult::Stopped(_) => return
+    };
+    while let StepperResult::Ok(_) = stepper.step(field, interpolator, callback) {}
+}
 
-impl Tracer3 {
-    pub fn trace<F, G, I, S, C>(field: &VectorField3<F, G>, interpolator: &I, mut stepper: S, start_position: &Point3<ftr>, callback: &mut C)
-    where F: num::Float + std::fmt::Display,
-          G: Grid3<F> + Clone,
-          I: Interpolator3,
-          S: Stepper3,
-          C: FnMut(&Point3<ftr>) -> StepperInstruction
-    {
-        match stepper.place(field, interpolator, start_position, callback) {
-            StepperResult::Ok(_) => {},
-            StepperResult::Stopped(_) => return
-        };
-        while let StepperResult::Ok(_) = stepper.step(field, interpolator, callback) {}
-    }
-
-    pub fn trace_dense_output<F, G, I, S, C>(field: &VectorField3<F, G>, interpolator: &I, mut stepper: S, start_position: &Point3<ftr>, callback: &mut C)
-    where F: num::Float + std::fmt::Display,
-          G: Grid3<F> + Clone,
-          I: Interpolator3,
-          S: Stepper3,
-          C: FnMut(&Point3<ftr>) -> StepperInstruction
-    {
-        match stepper.place(field, interpolator, start_position, callback) {
-            StepperResult::Ok(_) => {},
-            StepperResult::Stopped(_) => return
-        };
-        while let StepperResult::Ok(_) = stepper.step_dense_output(field, interpolator, callback) {}
-    }
+/// Traces a field line through the given vector field from the given start position,
+/// using the given interpolator and stepper. The given callback closure is called with
+/// regularly spaced positions along the field line.
+pub fn trace_dense<F, G, I, S, C>(field: &VectorField3<F, G>, interpolator: &I, mut stepper: S, start_position: &Point3<ftr>, callback: &mut C)
+where F: num::Float + std::fmt::Display,
+        G: Grid3<F> + Clone,
+        I: Interpolator3,
+        S: Stepper3,
+        C: FnMut(&Point3<ftr>) -> StepperInstruction
+{
+    match stepper.place(field, interpolator, start_position, callback) {
+        StepperResult::Ok(_) => {},
+        StepperResult::Stopped(_) => return
+    };
+    while let StepperResult::Ok(_) = stepper.step_dense_output(field, interpolator, callback) {}
 }
