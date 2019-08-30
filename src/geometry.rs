fa1fb35cae0f1e36efc7411b6cf25fee6396c079
Lars Frogner <lars_frogner@hotmail.com>
2019-08-30 09:15:16 +0200

    Implement more methods and operations for vectors and points

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,6 +1,7 @@
 //! Geometric utility objects.
 
 use num;
+use std::ops::{Index, IndexMut, Add, Sub, Mul, Div};
 use ndarray::prelude::*;
 
 /// Denotes the x- y- or z-dimension.
@@ -39,12 +40,12 @@ impl<T> In3D<T> {
     pub fn new(x: T, y: T, z: T) -> Self { In3D([x, y, z]) }
 }
 
-impl<T> std::ops::Index<Dim3> for In3D<T> {
+impl<T> Index<Dim3> for In3D<T> {
     type Output = T;
     fn index(&self, dim: Dim3) -> &Self::Output { &self.0[dim as usize] }
 }
 
-impl<T> std::ops::IndexMut<Dim3> for In3D<T> {
+impl<T> IndexMut<Dim3> for In3D<T> {
     fn index_mut(&mut self, dim: Dim3) -> &mut Self::Output { &mut self.0[dim as usize] }
 }
 
@@ -57,162 +58,321 @@ impl<T> In2D<T> {
     pub fn new(x: T, y: T) -> Self { In2D([x, y]) }
 }
 
-impl<T> std::ops::Index<Dim3> for In2D<T> {
+impl<T> Index<Dim3> for In2D<T> {
     type Output = T;
     fn index(&self, dim: Dim3) -> &Self::Output { &self.0[dim as usize] }
 }
 
-impl<T> std::ops::IndexMut<Dim3> for In2D<T> {
+impl<T> IndexMut<Dim3> for In2D<T> {
     fn index_mut(&mut self, dim: Dim3) -> &mut Self::Output { &mut self.0[dim as usize] }
 }
 
 /// A 3D vector.
 #[derive(Debug, Clone, PartialEq)]
-pub struct Vec3<T: num::Float>(In3D<T>);
+pub struct Vec3<F: num::Float>(In3D<F>);
 
-impl<T: num::Float> Vec3<T> {
+impl<F: num::Float> Vec3<F> {
     /// Creates a new 3D vector given the three components.
-    pub fn new(x: T, y: T, z: T) -> Self { Vec3(In3D::new(x, y, z)) }
+    pub fn new(x: F, y: F, z: F) -> Self { Vec3(In3D::new(x, y, z)) }
+
+    /// Creates a new zero vector.
+    pub fn zero() -> Self { Vec3::new(F::zero(), F::zero(), F::zero()) }
+
+    /// Creates a new vector from the given vector, which may have a different component type.
+    pub fn from<U: num::Float>(other: &Vec3<U>) -> Self {
+        Vec3::new(F::from(other[X]).unwrap(), F::from(other[Y]).unwrap(), F::from(other[Z]).unwrap())
+    }
+
+    /// Constructs a new point from the vector components.
+    pub fn to_point3(&self) -> Point3<F> {
+        Point3::new(self[X], self[Y], self[Z])
+    }
+
+    /// Constructs a new vector from the absolute values of the vector components.
+    pub fn abs(&self) -> Self {
+        Vec3::new(F::abs(self[X]), F::abs(self[Y]), F::abs(self[Z]))
+    }
+
+    /// Constructs a new vector by taking the component-wise max with the given vector.
+    pub fn max_with(&self, other: &Self) -> Self {
+        Vec3::new(F::max(self[X], other[X]), F::max(self[Y], other[Y]), F::max(self[Z], other[Z]))
+    }
+
+    /// Computes the squared length of the vector.
+    pub fn squared_length(&self) -> F {
+        self[X]*self[X] + self[Y]*self[Y] + self[Z]*self[Z]
+    }
+
+    /// Computes the length of the vector.
+    pub fn length(&self) -> F {
+        self.squared_length().sqrt()
+    }
+
+    /// Whether the vector is the zero vector.
+    pub fn is_zero(&self) -> bool {
+        self[X] == F::zero() && self[Y] == F::zero() && self[Z] == F::zero()
+    }
 
     /// Computes the dot product of the vector with another vector.
-    pub fn dot(&self, other: &Self) -> T {
+    pub fn dot(&self, other: &Self) -> F {
         self[X]*other[X] +
         self[Y]*other[Y] +
         self[Z]*other[Z]
     }
+
+    /// Normalizes the vector to have unit length.
+    pub fn normalize(&mut self) {
+        let length = self.length();
+        assert!(length != F::zero());
+        let inv_length = length.recip();
+        self[X] = self[X]*inv_length;
+        self[Y] = self[Y]*inv_length;
+        self[Z] = self[Z]*inv_length;
+    }
 }
 
-impl<T: num::Float> std::ops::Index<Dim3> for Vec3<T> {
-    type Output = T;
+impl<F: num::Float> Index<Dim3> for Vec3<F> {
+    type Output = F;
     fn index(&self, dim: Dim3) -> &Self::Output { &self.0[dim] }
 }
 
-impl<T: num::Float> std::ops::IndexMut<Dim3> for Vec3<T> {
+impl<F: num::Float> IndexMut<Dim3> for Vec3<F> {
     fn index_mut(&mut self, dim: Dim3) -> &mut Self::Output { &mut self.0[dim] }
 }
 
-impl<T: num::Float> std::ops::Add<Vec3<T>> for Vec3<T> {
-    type Output = Self;
-    fn add(self, other: Self) -> Self {
-        Vec3::new(self[X] + other[X],
-                  self[Y] + other[Y],
-                  self[Z] + other[Z])
+impl<'a, F: num::Float> Add<&'a Vec3<F>> for &'a Vec3<F> {
+    type Output = Vec3<F>;
+    fn add(self, other: Self) -> Self::Output {
+        Self::Output::new(self[X] + other[X],
+                          self[Y] + other[Y],
+                          self[Z] + other[Z])
     }
 }
 
-impl<T: num::Float> std::ops::Sub<Vec3<T>> for Vec3<T> {
+impl<F: num::Float> Add<Vec3<F>> for &Vec3<F> {
+    type Output = Vec3<F>;
+    fn add(self, other: Vec3<F>) -> Self::Output { self + &other }
+}
+
+impl<F: num::Float> Add<Vec3<F>> for Vec3<F> {
     type Output = Self;
-    fn sub(self, other: Self) -> Self {
-        Vec3::new(self[X] - other[X],
-                  self[Y] - other[Y],
-                  self[Z] - other[Z])
+    fn add(self, other: Self) -> Self::Output { &self + &other }
+}
+
+impl<F: num::Float> Add<&Vec3<F>> for Vec3<F> {
+    type Output = Self;
+    fn add(self, other: &Self) -> Self::Output { &self + other }
+}
+
+impl<'a, F: num::Float> Sub<&'a Vec3<F>> for &'a Vec3<F> {
+    type Output = Vec3<F>;
+    fn sub(self, other: Self) -> Self::Output {
+        Self::Output::new(self[X] - other[X],
+                          self[Y] - other[Y],
+                          self[Z] - other[Z])
     }
 }
 
-impl<T: num::Float> std::ops::Mul<T> for Vec3<T> {
+impl<F: num::Float> Sub<Vec3<F>> for &Vec3<F> {
+    type Output = Vec3<F>;
+    fn sub(self, other: Vec3<F>) -> Self::Output { self - &other }
+}
+
+impl<F: num::Float> Sub<Vec3<F>> for Vec3<F> {
     type Output = Self;
-    fn mul(self, factor: T) -> Self {
-        Vec3::new(factor*self[X],
-                  factor*self[Y],
-                  factor*self[Z])
+    fn sub(self, other: Self) -> Self::Output { &self - &other }
+}
+
+impl<F: num::Float> Sub<&Vec3<F>> for Vec3<F> {
+    type Output = Self;
+    fn sub(self, other: &Self) -> Self::Output { &self - other }
+}
+
+impl<F: num::Float> Mul<F> for &Vec3<F> {
+    type Output = Vec3<F>;
+    fn mul(self, factor: F) -> Self::Output {
+        Self::Output::new(factor*self[X],
+                          factor*self[Y],
+                          factor*self[Z])
     }
+}
+
+impl<F: num::Float> Mul<F> for Vec3<F> {
+    type Output = Self;
+    fn mul(self, factor: F) -> Self::Output { &self*factor }
+}
+
+impl<F: num::Float> Div<F> for &Vec3<F> {
+    type Output = Vec3<F>;
+    fn div(self, divisor: F) -> Self::Output {
+        #![allow(clippy::suspicious_arithmetic_impl)]
+        let factor = divisor.recip();
+        self*factor
+    }
+}
+
+impl<F: num::Float> Div<F> for Vec3<F> {
+    type Output = Self;
+    fn div(self, divisor: F) -> Self::Output { &self/divisor }
 }
 
 /// A 3D spatial coordinate.
 #[derive(Debug, Clone, PartialEq)]
-pub struct Point3<T: num::Float>(In3D<T>);
+pub struct Point3<F: num::Float>(In3D<F>);
 
-impl<T: num::Float> Point3<T> {
+impl<F: num::Float> Point3<F> {
     /// Creates a new 3D point given the three components.
-    pub fn new(x: T, y: T, z: T) -> Self { Point3(In3D::new(x, y, z)) }
+    pub fn new(x: F, y: F, z: F) -> Self { Point3(In3D::new(x, y, z)) }
+
+    /// Creates a new point from the given point, which may have a different component type.
+    pub fn from<U: num::Float>(other: &Point3<U>) -> Self {
+        Point3::new(F::from(other[X]).unwrap(), F::from(other[Y]).unwrap(), F::from(other[Z]).unwrap())
+    }
+
+    /// Constructs a new vector from the point components.
+    pub fn to_vec3(&self) -> Vec3<F> {
+        Vec3::new(self[X], self[Y], self[Z])
+    }
 
     /// Creates a new 3D point with all components set to zero.
-    pub fn origin() -> Self { Self::new(T::zero(), T::zero(), T::zero()) }
+    pub fn origin() -> Self { Self::new(F::zero(), F::zero(), F::zero()) }
 }
 
-impl<T: num::Float> std::ops::Index<Dim3> for Point3<T> {
-    type Output = T;
+impl<F: num::Float> Index<Dim3> for Point3<F> {
+    type Output = F;
     fn index(&self, dim: Dim3) -> &Self::Output { &self.0[dim] }
 }
 
-impl<T: num::Float> std::ops::IndexMut<Dim3> for Point3<T> {
+impl<F: num::Float> IndexMut<Dim3> for Point3<F> {
     fn index_mut(&mut self, dim: Dim3) -> &mut Self::Output { &mut self.0[dim] }
 }
 
-impl<T: num::Float> std::ops::Add<Vec3<T>> for Point3<T> {
-    type Output = Self;
-    fn add(self, vector: Vec3<T>) -> Self {
-        Point3::new(self[X] + vector[X],
-                    self[Y] + vector[Y],
-                    self[Z] + vector[Z])
+impl<'a, F: num::Float> Sub<&'a Self> for &'a Point3<F> {
+    type Output = Vec3<F>;
+    fn sub(self, other: &Self) -> Self::Output {
+        Self::Output::new(self[X] - other[X],
+                          self[Y] - other[Y],
+                          self[Z] - other[Z])
     }
 }
 
-impl<T: num::Float> std::ops::Sub<Point3<T>> for Point3<T> {
-    type Output = Vec3<T>;
-    fn sub(self, other: Self) -> Vec3<T> {
-        Vec3::new(self[X] - other[X],
-                  self[Y] - other[Y],
-                  self[Z] - other[Z])
+impl<F: num::Float> Sub<Self> for Point3<F> {
+    type Output = Vec3<F>;
+    fn sub(self, other: Self) -> Self::Output { &self - &other }
+}
+
+impl<F: num::Float> Sub<Self> for &Point3<F> {
+    type Output = Vec3<F>;
+    fn sub(self, other: Self) -> Self::Output { #![allow(clippy::op_ref)] self - &other }
+}
+
+impl<F: num::Float> Sub<&Self> for Point3<F> {
+    type Output = Vec3<F>;
+    fn sub(self, other: &Self) -> Self::Output { &self - other }
+}
+
+impl<'a, F: num::Float> Add<&'a Vec3<F>> for &'a Point3<F> {
+    type Output = Point3<F>;
+    fn add(self, vector: &Vec3<F>) -> Self::Output {
+        Self::Output::new(self[X] + vector[X],
+                          self[Y] + vector[Y],
+                          self[Z] + vector[Z])
     }
+}
+
+impl<F: num::Float> Add<Vec3<F>> for Point3<F> {
+    type Output = Self;
+    fn add(self, vector: Vec3<F>) -> Self::Output { &self + &vector }
+}
+
+impl<F: num::Float> Add<Vec3<F>> for &Point3<F> {
+    type Output = Point3<F>;
+    fn add(self, vector: Vec3<F>) -> Self::Output { self + &vector }
+}
+
+impl<'a, F: num::Float> Sub<&'a Vec3<F>> for &'a Point3<F> {
+    type Output = Point3<F>;
+    fn sub(self, vector: &Vec3<F>) -> Self::Output {
+        Self::Output::new(self[X] - vector[X],
+                          self[Y] - vector[Y],
+                          self[Z] - vector[Z])
+    }
+}
+
+impl<F: num::Float> Sub<Vec3<F>> for Point3<F> {
+    type Output = Self;
+    fn sub(self, vector: Vec3<F>) -> Self::Output { &self - &vector }
+}
+
+impl<F: num::Float> Sub<Vec3<F>> for &Point3<F> {
+    type Output = Point3<F>;
+    fn sub(self, vector: Vec3<F>) -> Self::Output { self - &vector }
 }
 
 /// A 3D index.
 #[derive(Debug, Clone, PartialEq)]
-pub struct Idx3<T: num::Integer>(In3D<T>);
+pub struct Idx3<I: num::Integer>(In3D<I>);
 
-impl<T: num::Integer> Idx3<T> {
+impl<I: num::Integer> Idx3<I> {
     /// Creates a new 3D index given the three components.
-    pub fn new(i: T, j: T, k: T) -> Self { Idx3(In3D::new(i, j, k)) }
+    pub fn new(i: I, j: I, k: I) -> Self { Idx3(In3D::new(i, j, k)) }
+
+    /// Creates a new 3D index from the given index, which may have a different component type.
+    pub fn from<U>(other: &Idx3<U>) -> Self
+    where I: num::NumCast + Copy,
+          U: num::Integer + num::NumCast + Copy
+    {
+        Idx3::new(I::from(other[X]).unwrap(), I::from(other[Y]).unwrap(), I::from(other[Z]).unwrap())
+    }
 }
 
-impl<T: num::Integer> std::ops::Index<Dim3> for Idx3<T> {
-    type Output = T;
+impl<I: num::Integer> Index<Dim3> for Idx3<I> {
+    type Output = I;
     fn index(&self, dim: Dim3) -> &Self::Output { &self.0[dim] }
 }
 
-impl<T: num::Integer> std::ops::IndexMut<Dim3> for Idx3<T> {
+impl<I: num::Integer> IndexMut<Dim3> for Idx3<I> {
     fn index_mut(&mut self, dim: Dim3) -> &mut Self::Output { &mut self.0[dim] }
 }
 
 /// 3D spatial coordinate arrays.
 #[derive(Debug, Clone)]
-pub struct Coords3<T: num::Float>(In3D<Array1<T>>);
+pub struct Coords3<F: num::Float>(In3D<Array1<F>>);
 
-impl<T: num::Float> Coords3<T> {
+impl<F: num::Float> Coords3<F> {
     /// Creates a new 3D set of coordinates given the component 1D coordinates.
-    pub fn new(x: Array1<T>, y: Array1<T>, z: Array1<T>) -> Self {
+    pub fn new(x: Array1<F>, y: Array1<F>, z: Array1<F>) -> Self {
         Coords3(In3D::new(x, y, z))
     }
 
     /// Creates a 3D point from the coordinates at the given index.
-    pub fn point(&self, idx: usize) -> Point3<T> {
+    pub fn point(&self, idx: usize) -> Point3<F> {
         Point3::new(self[X][idx], self[Y][idx], self[Y][idx])
     }
 }
 
-impl<T: num::Float> std::ops::Index<Dim3> for Coords3<T> {
-    type Output = Array1<T>;
+impl<F: num::Float> Index<Dim3> for Coords3<F> {
+    type Output = Array1<F>;
     fn index(&self, dim: Dim3) -> &Self::Output { &self.0[dim] }
 }
 
 /// References to 3D spatial coordinate arrays.
 #[derive(Debug, Clone, PartialEq)]
-pub struct CoordRefs3<'a, T: num::Float>(In3D<&'a Array1<T>>);
+pub struct CoordRefs3<'a, F: num::Float>(In3D<&'a Array1<F>>);
 
-impl<'a, T: num::Float> CoordRefs3<'a, T> {
+impl<'a, F: num::Float> CoordRefs3<'a, F> {
     /// Creates a new 3D set of coordinate references given references to the component arrays.
-    pub fn new(x: &'a Array1<T>, y: &'a Array1<T>, z: &'a Array1<T>) -> Self {
+    pub fn new(x: &'a Array1<F>, y: &'a Array1<F>, z: &'a Array1<F>) -> Self {
         CoordRefs3(In3D::new(x, y, z))
     }
 
     /// Creates a 3D point from the coordinates at the given index.
-    pub fn point(&self, idx: usize) -> Point3<T> {
+    pub fn point(&self, idx: usize) -> Point3<F> {
         Point3::new(self[X][idx], self[Y][idx], self[Y][idx])
     }
 }
 
-impl<'a, T: num::Float> std::ops::Index<Dim3> for CoordRefs3<'a, T> {
-    type Output = &'a Array1<T>;
+impl<'a, F: num::Float> Index<Dim3> for CoordRefs3<'a, F> {
+    type Output = &'a Array1<F>;
     fn index(&self, dim: Dim3) -> &Self::Output { &self.0[dim] }
 }
