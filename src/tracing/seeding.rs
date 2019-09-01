da03da8c1a44d80d5a216a16db26b6dea6a59eff
Lars Frogner <lars_frogner@hotmail.com>
2019-09-01 17:20:48 +0200

    Define seeder trait

---
+++ src/tracing/seeding.rs
@@ -0,0 +1,10 @@
+//! Generation of seed points for field line tracing.
+
+use crate::geometry::Point3;
+use super::ftr;
+
+/// Defines the properties of a 3D seed point generator.
+pub trait Seeder3: IntoIterator<Item = Point3<ftr>> {}
+
+// Let a vector of points work as a seeder.
+impl Seeder3 for Vec<Point3<ftr>> {}
