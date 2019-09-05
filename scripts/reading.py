e4217d99e7f1104740fef30ad4e4a43653700811
Lars Frogner <lars_frogner@hotmail.com>
2019-09-06 00:37:12 +0200

    Update for parsing new field line set format

--- scripts/reading.py
+++ scripts/reading.py
@@ -23,22 +23,18 @@ def read_3d_field_line(file_path):
 
 
 def read_3d_field_line_set(file_path):
-    field_line_set = FieldLineSet3([])
     with file_path.open(mode='rb') as f:
-        while True:
-            try:
-                data = pickle.load(f)
-            except EOFError:
-                break
-            field_line = __parse_fieldline3(data)
-            field_line_set.insert(field_line)
-    return field_line_set
+        data = pickle.load(f)
+    return __parse_fieldlineset3(data)
 
 
 def __parse_scalarfield2(data):
     coords = __parse_coords2(data['coords'])
     values = __parse_ndarray(data['values'])
     return ScalarField2(coords, values)
+
+def __parse_fieldlineset3(data):
+    return FieldLineSet3([__parse_fieldline3(d) for d in data['field_lines']])
 
 def __parse_fieldline3(data):
     positions = __parse_vec_of_vec3(data['positions'])
