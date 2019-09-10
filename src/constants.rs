0fd088df79345255039a86dabba38092ab1988b2
Lars Frogner <lars_frogner@hotmail.com>
2019-09-10 00:21:28 -0700

    Add constants

--- src/constants.rs
+++ src/constants.rs
@@ -12,38 +12,42 @@ pub const PI: fcn = 3.14159;
 // Physical constants
 
 /// Atomic mass unit [g].
-pub const AMU:        fcn = 1.660_540_2e-24;
+pub const AMU:          fcn = 1.660_540_2e-24;
 /// Electron charge [esu].
-pub const Q_ELECTRON: fcn = 4.80325e-10;
+pub const Q_ELECTRON:   fcn = 4.80325e-10;
 /// Electron mass [g].
-pub const M_ELECTRON: fcn = 9.109_389_7e-28;
+pub const M_ELECTRON:   fcn = 9.109_389_7e-28;
 /// Hydrogen mass [g].
-pub const M_H:        fcn = 1.672_621_9e-24;
+pub const M_H:          fcn = 1.672_621_9e-24;
 /// Helium mass [g].
-pub const M_HE:       fcn = 6.65e-24;
+pub const M_HE:         fcn = 6.65e-24;
 /// Ion mass [g].
-pub const MION:       fcn  = M_H;
+pub const MION:         fcn  = M_H;
 /// Speed of light in vacuum [cm/s].
-pub const CLIGHT:     fcn = 2.997_924_58e10;
+pub const CLIGHT:       fcn = 2.997_924_58e10;
 /// Boltzmann constant [erg/K].
-pub const KBOLTZMANN: fcn = 1.380_658e-16;
+pub const KBOLTZMANN:   fcn = 1.380_658e-16;
 /// Planck constant [erg s].
-pub const HPLANCK:    fcn = 6.626_075_5e-27;
+pub const HPLANCK:      fcn = 6.626_075_5e-27;
 /// Stefan-Boltzmann constant [erg/(cm^2 s K^4)].
-pub const STEFBOLTZ:  fcn = 5.670_400e-5;
+pub const STEFBOLTZ:    fcn = 5.670_400e-5;
 /// Bohr radius [cm].
-pub const RBOHR:      fcn = 5.291_773_49e-9;
+pub const RBOHR:        fcn = 5.291_773_49e-9;
 /// Ionization potential of hydrogen [erg].
-pub const E_RYDBERG:  fcn = 2.179_874_1e-11;
+pub const E_RYDBERG:    fcn = 2.179_874_1e-11;
 /// H2 dissociation energy [eV].
-pub const EH2DISS:    fcn = 4.478;
+pub const EH2DISS:      fcn = 4.478;
 /// pi e^2 / m_e c [cm^2 Hz].
-pub const PIE2_MEC:   fcn = 0.02654;
+pub const PIE2_MEC:     fcn = 0.02654;
+/// Electron rest energy [erg].
+pub const MC2_ELECTRON: fcn = M_ELECTRON*CLIGHT*CLIGHT;
 
 // Unit conversion factors
 
 /// Conversion factor from electron volts to ergs.
 pub const EV_TO_ERG:          fcn = 1.602_177_33e-12;
+/// Conversion factor from kilo electron volts to ergs.
+pub const KEV_TO_ERG:         fcn = EV_TO_ERG*1e3;
 /// Conversion factor from electron volts to kelvin (1/`KBOLTZMANN`).
 pub const EV_TO_K:            fcn =  11604.50520;
 /// Conversion factor from electron volts to Joules.
