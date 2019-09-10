fc593fbf22cff77417cf9a9a5071b77de462743b
Lars Frogner <lars_frogner@hotmail.com>
2019-09-10 00:20:26 -0700

    Add type alias for ebeam floating point precision

--- src/ebeam.rs
+++ src/ebeam.rs
@@ -1,3 +1,7 @@
 //! Non-thermal electron beam physics in Bifrost simulations.
 
 pub mod distribution;
+
+/// Floating-point precision to use for electron beam physics.
+#[allow(non_camel_case_types)]
+pub type feb = f64;
