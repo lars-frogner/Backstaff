165283a4056db298ec1a9bc149ec9682596c3085
Lars Frogner <lars_frogner@hotmail.com>
2019-09-10 00:07:06 -0700

    Replace unwrap with expect

--- src/random.rs
+++ src/random.rs
@@ -21,7 +21,7 @@ where F: BFloat + SampleUniform
     let rng = rand::thread_rng();
     uniform_cdf_values.sample_iter(rng).take(n_samples).map(
         |sampled_cdf_value| {
-            match cdf.binary_search_by(|cdf_value| cdf_value.partial_cmp(&sampled_cdf_value).unwrap()) {
+            match cdf.binary_search_by(|cdf_value| cdf_value.partial_cmp(&sampled_cdf_value).expect("Float comparison failed.")) {
                 Result::Ok(exact_idx) => exact_idx,
                 Result::Err(adjacent_idx) => adjacent_idx
             }
