99395c316acc115e7ebacf6303242622de00d1aa
Lars Frogner <lars_frogner@hotmail.com>
2019-09-30 21:30:23 +0200

    Implement From traits for conversion between Verbose and bool

--- src/io.rs
+++ src/io.rs
@@ -18,11 +18,28 @@ pub enum Verbose {
 }
 
 impl Verbose {
+    /// Whether verbosity is activated.
     #[allow(clippy::trivially_copy_pass_by_ref)]
     pub fn is_yes(&self) -> bool {
         match *self {
             Verbose::Yes => true,
             Verbose::No => false,
         }
+    }
+}
+
+impl From<bool> for Verbose {
+    fn from(is_verbose: bool) -> Self {
+        if is_verbose {
+            Verbose::Yes
+        } else {
+            Verbose::No
+        }
+    }
+}
+
+impl From<Verbose> for bool {
+    fn from(verbose: Verbose) -> Self {
+        verbose.is_yes()
     }
 }
