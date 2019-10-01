71f71f230ad95c5653a15033934291023f0c826d
Lars Frogner <lars_frogner@hotmail.com>
2019-10-01 20:56:38 +0200

    Move top-level CLI code to cli module

--- src/bin/bifrost.rs
+++ src/bin/bifrost.rs
@@ -1,24 +1,5 @@
 //! Command line runner for the `bifrost` library.
 
-use bifrost::cli::{ebeam, snapshot};
-use clap::{self, App, AppSettings};
-
 fn main() {
-    let app = App::new(clap::crate_name!())
-        .version(clap::crate_version!())
-        .author(clap::crate_authors!())
-        .about(clap::crate_description!())
-        .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(snapshot::build_subcommand_snapshot())
-        .subcommand(ebeam::build_subcommand_ebeam());
-
-    let arguments = app.get_matches();
-
-    if let Some(snapshot_arguments) = arguments.subcommand_matches("snapshot") {
-        snapshot::run_subcommand_snapshot(snapshot_arguments);
-    }
-
-    if let Some(ebeam_arguments) = arguments.subcommand_matches("ebeam") {
-        ebeam::run_subcommand_ebeam(ebeam_arguments);
-    }
+    bifrost::cli::run();
 }
--- src/cli.rs
+++ src/cli.rs
@@ -2,3 +2,26 @@
 
 pub mod ebeam;
 pub mod snapshot;
+
+use clap::{self, App, AppSettings};
+
+/// Runs the `bifrost` command line program.
+pub fn run() {
+    let app = App::new(clap::crate_name!())
+        .version(clap::crate_version!())
+        .author(clap::crate_authors!())
+        .about(clap::crate_description!())
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .subcommand(snapshot::build_subcommand_snapshot())
+        .subcommand(ebeam::build_subcommand_ebeam());
+
+    let arguments = app.get_matches();
+
+    if let Some(snapshot_arguments) = arguments.subcommand_matches("snapshot") {
+        snapshot::run_subcommand_snapshot(snapshot_arguments);
+    }
+
+    if let Some(ebeam_arguments) = arguments.subcommand_matches("ebeam") {
+        ebeam::run_subcommand_ebeam(ebeam_arguments);
+    }
+}
