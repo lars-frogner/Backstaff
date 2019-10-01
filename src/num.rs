dc160f63f68ca818602fee9332b4b53cc24f9ff9
Lars Frogner <lars_frogner@hotmail.com>
2019-10-01 20:36:10 +0200

    Add struct for ordering of index-value pairs

--- src/num.rs
+++ src/num.rs
@@ -2,7 +2,7 @@
 
 use ieee754;
 use num;
-use std::fmt;
+use std::{cmp, fmt};
 
 /// Floating point marker trait for easier control over trait bounds.
 pub trait BFloat:
@@ -12,3 +12,27 @@ pub trait BFloat:
 
 impl BFloat for f32 {}
 impl BFloat for f64 {}
+
+/// Integer-float pair that can be ordered based on the float.
+pub struct OrderableIndexValuePair<I: num::Integer, F: BFloat>(pub I, pub F);
+
+impl<I: num::Integer, F: BFloat> PartialEq for OrderableIndexValuePair<I, F> {
+    fn eq(&self, other: &Self) -> bool {
+        self.1 == other.1
+    }
+}
+
+impl<I: num::Integer, F: BFloat> PartialOrd for OrderableIndexValuePair<I, F> {
+    fn partial_cmp(&self, other: &Self) -> Option<cmp::Ordering> {
+        self.1.partial_cmp(&other.1)
+    }
+}
+
+impl<I: num::Integer, F: BFloat> Eq for OrderableIndexValuePair<I, F> {}
+
+impl<I: num::Integer, F: BFloat> Ord for OrderableIndexValuePair<I, F> {
+    fn cmp(&self, other: &Self) -> cmp::Ordering {
+        self.partial_cmp(&other)
+            .expect("NaN in floating point comparison.")
+    }
+}
