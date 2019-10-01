81f04cdffc33d527a7331d31b8a5493401687475
Lars Frogner <lars_frogner@hotmail.com>
2019-10-02 00:24:07 +0200

    Add option to perform timing

--- src/cli.rs
+++ src/cli.rs
@@ -3,7 +3,8 @@
 pub mod ebeam;
 pub mod snapshot;
 
-use clap::{self, App, AppSettings};
+use clap::{self, App, AppSettings, Arg};
+use std::time::Instant;
 
 /// Runs the `bifrost` command line program.
 pub fn run() {
@@ -12,10 +13,18 @@ pub fn run() {
         .author(clap::crate_authors!())
         .about(clap::crate_description!())
         .setting(AppSettings::SubcommandRequiredElseHelp)
+        .arg(
+            Arg::with_name("timing")
+                .short("t")
+                .long("timing")
+                .help("Display elapsed time when done"),
+        )
         .subcommand(snapshot::build_subcommand_snapshot())
         .subcommand(ebeam::build_subcommand_ebeam());
 
     let arguments = app.get_matches();
+
+    let start_instant = Instant::now();
 
     if let Some(snapshot_arguments) = arguments.subcommand_matches("snapshot") {
         snapshot::run_subcommand_snapshot(snapshot_arguments);
@@ -23,5 +32,9 @@ pub fn run() {
 
     if let Some(ebeam_arguments) = arguments.subcommand_matches("ebeam") {
         ebeam::run_subcommand_ebeam(ebeam_arguments);
+    }
+
+    if arguments.is_present("timing") {
+        println!("Elapsed time: {} s", start_instant.elapsed().as_secs_f64());
     }
 }
