df9184700f424fde78e1be2f9655a41dbaa76879
Lars Frogner <lars_frogner@hotmail.com>
2019-10-06 15:41:44 +0200

    Add more argument parsing utility functions

--- src/cli.rs
+++ src/cli.rs
@@ -102,8 +102,27 @@ where
     <T as std::str::FromStr>::Err: std::fmt::Display,
     D: Fn() -> T,
 {
-    if let Some(value_str) = arguments.value_of(argument_name) {
-        parse_value_string(argument_name, value_str)
+    if let Some(value_string) = arguments.value_of(argument_name) {
+        parse_value_string(argument_name, value_string)
+    } else {
+        default_constructor()
+    }
+}
+
+fn get_values_from_parseable_argument_with_custom_defaults<T, D>(
+    arguments: &ArgMatches,
+    argument_name: &str,
+    default_constructor: &D,
+) -> Vec<T>
+where
+    T: std::str::FromStr,
+    <T as std::str::FromStr>::Err: std::fmt::Display,
+    D: Fn() -> Vec<T>,
+{
+    if let Some(value_strings) = arguments.values_of(argument_name) {
+        value_strings
+            .map(|value_string| parse_value_string(argument_name, value_string))
+            .collect()
     } else {
         default_constructor()
     }
@@ -113,7 +132,7 @@ where
 fn get_value_from_constrained_argument_with_custom_default<T, D>(
     arguments: &ArgMatches,
     argument_name: &str,
-    possible_value_strs: &[&str],
+    possible_value_strings: &[&str],
     possible_values: &[T],
     default_constructor: &D,
 ) -> T
@@ -121,16 +140,17 @@ where
     T: Copy,
     D: Fn() -> T,
 {
-    if let Some(value_str) = arguments.value_of(argument_name) {
+    if let Some(value_string) = arguments.value_of(argument_name) {
         let mut value: Option<T> = None;
-        for (possible_value_str, possible_value) in possible_value_strs.iter().zip(possible_values)
+        for (possible_value_string, possible_value) in
+            possible_value_strings.iter().zip(possible_values)
         {
-            if *possible_value_str == value_str {
+            if *possible_value_string == value_string {
                 value = Some(*possible_value);
                 break;
             }
         }
-        value.unwrap_or_else(|| panic!("Invalid {}: {}", argument_name, value_str))
+        value.unwrap_or_else(|| panic!("Invalid {}: {}", argument_name, value_string))
     } else {
         default_constructor()
     }
@@ -139,23 +159,25 @@ where
 fn get_value_from_required_constrained_argument<T>(
     arguments: &ArgMatches,
     argument_name: &str,
-    possible_value_strs: &[&str],
+    possible_value_strings: &[&str],
     possible_values: &[T],
 ) -> T
 where
     T: Copy,
 {
-    let value_str = arguments
+    let value_string = arguments
         .value_of(argument_name)
         .expect("No value for required argument.");
     let mut value: Option<T> = None;
-    for (possible_value_str, possible_value) in possible_value_strs.iter().zip(possible_values) {
-        if *possible_value_str == value_str {
+    for (possible_value_string, possible_value) in
+        possible_value_strings.iter().zip(possible_values)
+    {
+        if *possible_value_string == value_string {
             value = Some(*possible_value);
             break;
         }
     }
-    value.unwrap_or_else(|| panic!("Invalid {}: {}", argument_name, value_str))
+    value.unwrap_or_else(|| panic!("Invalid {}: {}", argument_name, value_string))
 }
 
 #[allow(dead_code)]
@@ -195,5 +217,36 @@ where
             conversion_mapping,
             default_value,
         )
+    })
+}
+
+fn get_values_from_param_file_argument_with_defaults<G, T, C>(
+    reader: &SnapshotReader3<G>,
+    arguments: &ArgMatches,
+    argument_name: &str,
+    param_file_argument_names: &[&str],
+    conversion_mapping: &C,
+    default_values: &[T],
+) -> Vec<T>
+where
+    G: Grid3<fdt>,
+    T: num::Num + str::FromStr + std::fmt::Display + Copy,
+    T::Err: string::ToString,
+    <T as str::FromStr>::Err: std::fmt::Display,
+    C: Fn(T) -> T,
+{
+    get_values_from_parseable_argument_with_custom_defaults(arguments, argument_name, &|| {
+        param_file_argument_names
+            .iter()
+            .zip(default_values)
+            .map(|(&param_file_argument_name, &default_value)| {
+                reader.get_converted_numerical_param_or_fallback_to_default_with_warning(
+                    argument_name,
+                    param_file_argument_name,
+                    conversion_mapping,
+                    default_value,
+                )
+            })
+            .collect()
     })
 }
