a01156ea091daf179ebc7bd02c3ab3f90d0e9baf
Lars Frogner <lars_frogner@hotmail.com>
2019-10-07 09:31:38 +0200

    Add convenience function to run command line version of crate from Python

---
+++ scripts/running.py
@@ -0,0 +1,20 @@
+import subprocess
+
+
+def run_bifrost_rust(*args):
+    args = ['cargo', 'run', '--release', '--'] + list(args)
+    print(' '.join(args))
+    process = subprocess.Popen(args,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    while True:
+        output = process.stdout.readline().decode()
+        error_msg = process.stderr.readline().decode()
+        if output == '' and error_msg == '' and process.poll() is not None:
+            break
+        if output:
+            print(output.strip())
+        if error_msg:
+            print(error_msg.strip())
+    return_code = process.poll()
+    return return_code
