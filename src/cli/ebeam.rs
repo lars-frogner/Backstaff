80c81f3ffaeabf8f01b2759810e047a7fb04f600
Lars Frogner <lars_frogner@hotmail.com>
2019-10-09 19:15:48 +0200

    Rename functions for consistency

--- src/cli/ebeam.rs
+++ src/cli/ebeam.rs
@@ -10,19 +10,19 @@ use crate::io::snapshot::{fdt, SnapshotCacher3};
 use clap::{App, AppSettings, ArgMatches, SubCommand};
 
 /// Builds a representation of the `ebeam` command line subcommand.
-pub fn build_subcommand_ebeam<'a, 'b>() -> App<'a, 'b> {
+pub fn create_ebeam_subcommand<'a, 'b>() -> App<'a, 'b> {
     SubCommand::with_name("ebeam")
         .about("Perform actions related to electron beams in the snapshot")
         .setting(AppSettings::SubcommandRequiredElseHelp)
-        .subcommand(simulate::build_subcommand_simulate())
+        .subcommand(simulate::create_simulate_subcommand())
 }
 
 /// Runs the actions for the `ebeam` subcommand using the given arguments.
-pub fn run_subcommand_ebeam<G: Grid3<fdt>>(
+pub fn run_ebeam_subcommand<G: Grid3<fdt>>(
     arguments: &ArgMatches,
     snapshot: &mut SnapshotCacher3<G>,
 ) {
     if let Some(simulate_arguments) = arguments.subcommand_matches("simulate") {
-        simulate::run_subcommand_simulate(simulate_arguments, snapshot);
+        simulate::run_simulate_subcommand(simulate_arguments, snapshot);
     }
 }
