5ef24d7cfa0cc53f7e1e835d991a5f1efc47fc64
Lars Frogner <lars_frogner@hotmail.com>
2019-11-03 11:02:14 +0100

    Add function for running remotely

--- bifrust/running.py
+++ bifrust/running.py
@@ -1,15 +1,15 @@
 import subprocess
 
 
-def run_bifrust(*args, features=['cli']):
-    args = [
-        'cargo', 'run', '--release', '--no-default-features', '--features',
-        ' '.join(features), '--'
-    ] + list(args)
-    print(' '.join(args))
+def run_command(*args, return_immediately=False):
     process = subprocess.Popen(args,
                                stdout=subprocess.PIPE,
                                stderr=subprocess.PIPE)
+    print(subprocess.list2cmdline(process.args))
+
+    if return_immediately:
+        return
+
     while True:
         output = process.stdout.readline().decode()
         error_msg = process.stderr.readline().decode()
@@ -21,3 +21,29 @@ def run_bifrust(*args, features=['cli']):
             print(error_msg.strip())
     return_code = process.poll()
     return return_code
+
+
+def run_bifrust(*args, pre_cargo_args=[], features=['cli']):
+    args = pre_cargo_args + [
+        'cargo', 'run', '--release', '--no-default-features', '--features',
+        ' '.join(features), '--'
+    ] + list(args)
+    return run_command(*args)
+
+
+def run_bifrust_remotely(destination_machine,
+                         destination_dir,
+                         *args,
+                         pre_ssh_args=[],
+                         pre_cargo_args=['nice'],
+                         features=['cli']):
+    run_command_args = pre_cargo_args + [
+        'cargo', 'run', '--release', '--no-default-features', '--features',
+        ' '.join(features), '--'
+    ] + list(args)
+    remote_command_args = ['cd', destination_dir, '&&', 'git', 'pull', '&&'
+                           ] + run_command_args
+    args = pre_ssh_args + [
+        'ssh', destination_machine, ' '.join(remote_command_args)
+    ]
+    return run_command(*args)
