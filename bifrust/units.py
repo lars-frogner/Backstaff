1638dfd7e5a2502b0574cb3bf3b9734792ce0e31
Lars Frogner <lars_frogner@hotmail.com>
2019-11-09 16:28:54 +0100

    Add constants and unit conversion factors

--- bifrust/units.py
+++ bifrust/units.py
@@ -1,5 +1,70 @@
 import numpy as np
 
+# Atomic mass unit [g].
+AMU = 1.660_540_2e-24
+# Electron charge [esu].
+Q_ELECTRON = 4.80325e-10
+# Electron mass [g].
+M_ELECTRON = 9.109_389_7e-28
+# Hydrogen mass [g].
+M_H = 1.672_621_9e-24
+# Helium mass [g].
+M_HE = 6.65e-24
+# Ion mass [g].
+MION = M_H
+# Speed of light in vacuum [cm/s].
+CLIGHT = 2.997_924_58e10
+# Boltzmann constant [erg/K].
+KBOLTZMANN = 1.380_658e-16
+# Planck constant [erg s].
+HPLANCK = 6.626_075_5e-27
+# Stefan-Boltzmann constant [erg/(cm^2 s K^4)].
+STEFBOLTZ = 5.670_400e-5
+# Bohr radius [cm].
+RBOHR = 5.291_773_49e-9
+# Ionization potential of hydrogen [erg].
+E_RYDBERG = 2.179_874_1e-11
+# H2 dissociation energy [eV].
+EH2DISS = 4.478
+# pi e^2 / m_e c [cm^2 Hz].
+PIE2_MEC = 0.02654
+# Electron rest energy [erg].
+MC2_ELECTRON = M_ELECTRON*CLIGHT*CLIGHT
+
+# Conversion factor from electron volts to ergs.
+EV_TO_ERG = 1.602_177_33e-12
+# Conversion factor from kilo electron volts to ergs.
+KEV_TO_ERG = EV_TO_ERG*1e3
+# Conversion factor from electron volts to kelvin (1/KBOLTZMANN).
+EV_TO_K = 11604.50520
+# Conversion factor from electron volts to Joules.
+EV_TO_J = 1.602_177_33e-19
+# Conversion factor from nanometers to meters.
+NM_TO_M = 1e-9
+# Conversion factor from centimeters to meters.
+CM_TO_M = 1e-2
+# Conversion factor from kilometers to meters.
+KM_TO_M = 1e3
+# Conversion factor from ergs to Joules.
+ERG_TO_JOULE = 1e-7
+# Conversion factor from grams to kilograms.
+G_TO_KG = 1e-3
+# Conversion factor from microns to nanometers.
+MICRON_TO_NM = 1e3
+# Conversion factor from megabarns to square meters.
+MEGABARN_TO_M2 = 1e-22
+# Conversion factor from atmospheres to Pascal.
+ATM_TO_PA = 1.0135e5
+# Conversion factor from dynes per square centimeter to Pascal.
+DYNE_CM2_TO_PASCAL = 0.1
+
+# Fraction of a mass of plasma assumed to be made up of hydrogen.
+HYDROGEN_MASS_FRACTION = 0.735
+# Conversion factor from mass density [g] to electron density [1/cm^3],
+# assuming a fully ionized plasma with no metals and the hard-coded value for
+# the hydrogen mass fraction.
+MASS_DENSITY_TO_ELECTRON_DENSITY = (1.0 + HYDROGEN_MASS_FRACTION)/(2.0*AMU)
+
 # Unit for length [cm]
 U_L = 1e8
 # Unit for time [s]
@@ -22,3 +87,5 @@ U_TE = U_E/U_T*U_L
 U_L3 = U_L*U_L*U_L
 # Unit for magnetic flux density [gauss]
 U_B = U_U*np.sqrt(4.0*np.pi*U_R)
+# Unit for electric field strength [statV/cm].
+U_EL = U_B*CLIGHT
