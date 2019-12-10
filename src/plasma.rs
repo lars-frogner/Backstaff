1a9540f1998145621bf34915791ca9e925240f9b
Lars Frogner <lars_frogner@hotmail.com>
2019-12-11 00:03:51 +0100

    Add module for plasma physics

---
+++ src/plasma.rs
@@ -0,0 +1,7 @@
+//! Plasma physics in Bifrost simulations.
+
+pub mod ionization;
+
+/// Floating-point precision to use for plasma physics.
+#[allow(non_camel_case_types)]
+pub type fpl = f64;
---
+++ src/plasma/ionization.rs
@@ -0,0 +1,24 @@
+//! Calculations of the ionization state of plasma particles.
+
+use super::fpl;
+use crate::constants::{EV_TO_ERG, HPLANCK, KBOLTZMANN, M_ELECTRON, M_H, PI, XI_H};
+use lazy_static::lazy_static;
+
+lazy_static! {
+    /// Constant factor in the Saha equations: (h^2/(2*pi*me*kB))^3/2
+    static ref SAHA_SCALE: fpl = fpl::powf(HPLANCK*HPLANCK/(2.0*PI*M_ELECTRON*KBOLTZMANN), 1.5);
+}
+
+/// Computes the number density of neutral hydrogen assuming thermal equilibrium.
+///
+/// The result is obtained by evaluating the Saha ionization equation.
+pub fn compute_equilibrium_neutral_hydrogen_density(
+    hydrogen_mass_fraction: fpl,
+    mass_density: fpl,
+    temperature: fpl,
+    electron_density: fpl,
+) -> fpl {
+    let tmp = electron_density * (*SAHA_SCALE) / fpl::powf(temperature, 1.5);
+    mass_density * hydrogen_mass_fraction * tmp
+        / (M_H * (tmp + fpl::exp(-XI_H * EV_TO_ERG / (KBOLTZMANN * temperature))))
+}
