c5de67c97075c3cdbba03967a2813680b0aee468
Lars Frogner <lars_frogner@hotmail.com>
2020-01-09 10:37:39 +0100

    Add function for computing ionization fraction

--- src/plasma/ionization.rs
+++ src/plasma/ionization.rs
@@ -9,7 +9,7 @@ lazy_static! {
     static ref SAHA_SCALE: fpl = fpl::powf(HPLANCK*HPLANCK/(2.0*PI*M_ELECTRON*KBOLTZMANN), 1.5);
 }
 
-/// Computes the number density of neutral hydrogen assuming thermal equilibrium.
+/// Computes the number density of neutral hydrogen, assuming thermal equilibrium.
 ///
 /// The result is obtained by evaluating the Saha ionization equation.
 pub fn compute_equilibrium_neutral_hydrogen_density(
@@ -21,4 +21,15 @@ pub fn compute_equilibrium_neutral_hydrogen_density(
     let tmp = electron_density * (*SAHA_SCALE) / fpl::powf(temperature, 1.5);
     mass_density * hydrogen_mass_fraction * tmp
         / (M_H * (tmp + fpl::exp(-XI_H * EV_TO_ERG / (KBOLTZMANN * temperature))))
+}
+
+/// Computes the fraction of hydrogen that is ionized, assuming thermal equilibrium.
+///
+/// The result is obtained by evaluating the Saha ionization equation.
+pub fn compute_equilibrium_hydrogen_ionization_fraction(
+    temperature: fpl,
+    electron_density: fpl,
+) -> fpl {
+    let tmp = electron_density * (*SAHA_SCALE) / fpl::powf(temperature, 1.5);
+    1.0 / (1.0 + tmp * fpl::exp(XI_H * EV_TO_ERG / (KBOLTZMANN * temperature)))
 }
