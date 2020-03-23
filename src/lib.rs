bb68c386d2c21fbab2bf1d47efc9c1416d50063c
Lars Frogner <lars_frogner@hotmail.com>
2020-03-23 10:18:25 +0100

    Add macros for exiting with error message

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,34 @@
 //! Useful tools for working with Bifrost in Rust.
 
+#[macro_export]
+macro_rules! exit_on_error {
+    ($result:expr, $($fmt_arg:tt)*) => {
+        match $result {
+            Ok(value) => value,
+            Err(err) => {
+                exit_with_error!($($fmt_arg)*, err.to_string())
+            }
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! exit_on_false {
+    ($logic:expr, $($print_arg:tt)*) => {
+        if !$logic {
+            exit_with_error!($($print_arg)*)
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! exit_with_error {
+    ($($print_arg:tt)*) => {{
+        eprintln!($($print_arg)*);
+        ::std::process::exit(1)
+    }};
+}
+
 pub mod constants;
 pub mod field;
 pub mod geometry;
