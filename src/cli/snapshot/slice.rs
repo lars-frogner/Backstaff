0f748ab62d02e3cc120f86ce0149d1d2dc2e0797
Lars Frogner <lars_frogner@hotmail.com>
2020-03-31 15:30:56 +0200

    Add enum for output type and atomic output support

--- src/cli/snapshot/slice.rs
+++ src/cli/snapshot/slice.rs
@@ -15,11 +15,15 @@ use crate::{
     interpolation::poly_fit::{PolyFitInterpolator3, PolyFitInterpolatorConfig},
     io::{
         snapshot::{self, fdt, SnapshotCacher3, SnapshotReader3},
-        utils as io_utils,
+        utils::AtomicOutputPath,
     },
 };
 use clap::{App, Arg, ArgMatches, SubCommand};
-use std::{path::PathBuf, str::FromStr};
+use std::{
+    fmt,
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 
 /// Builds a representation of the `snapshot-slice` command line subcommand.
 pub fn create_slice_subcommand<'a, 'b>() -> App<'a, 'b> {
@@ -45,7 +49,7 @@ pub fn create_slice_subcommand<'a, 'b>() -> App<'a, 'b> {
         .arg(
             Arg::with_name("overwrite")
                 .long("overwrite")
-                .help("Automatically overwrite any existing file"),
+                .help("Automatically overwrite any existing file (unless listed as protected)"),
         )
         .arg(
             Arg::with_name("quantity")
@@ -106,6 +110,7 @@ pub fn run_slice_subcommand<G, R>(
     arguments: &ArgMatches,
     snapshot: &mut SnapshotCacher3<G, R>,
     snap_num_offset: Option<u32>,
+    protected_file_types: &[&str],
 ) where
     G: Grid3<fdt>,
     R: SnapshotReader3<G>,
@@ -129,11 +134,7 @@ pub fn run_slice_subcommand<G, R>(
         "Error: Could not interpret path to output file: {}"
     );
 
-    let output_extension = "pickle";
-
-    if output_file_path.extension().is_none() {
-        output_file_path.set_extension(output_extension);
-    }
+    let output_type = OutputType::from_path(&output_file_path);
 
     if let Some(snap_num_offset) = snap_num_offset {
         let (output_base_name, output_existing_num) =
@@ -142,15 +143,17 @@ pub fn run_slice_subcommand<G, R>(
         output_file_path.set_file_name(snapshot::create_snapshot_file_name(
             &output_base_name,
             output_existing_num + snap_num_offset,
-            output_extension,
+            &output_type.to_string(),
         ));
     }
 
-    let force_overwrite = arguments.is_present("overwrite");
+    let automatic_overwrite = arguments.is_present("overwrite");
 
-    if !force_overwrite && !io_utils::write_allowed(&output_file_path) {
-        exit_with_error!("Aborted");
-    }
+    let atomic_output_path = exit_on_error!(
+        AtomicOutputPath::new(output_file_path),
+        "Error: Could not create temporary output file: {}"
+    );
+    atomic_output_path.ensure_write_allowed(automatic_overwrite, protected_file_types);
 
     let sample_location = arguments
         .value_of("sample-location")
@@ -184,17 +187,17 @@ pub fn run_slice_subcommand<G, R>(
                 "x" => {
                     field
                         .slice_across_x(&interpolator, coord, resampled_coord_locations)
-                        .save_as_pickle(output_file_path)
+                        .save_as_pickle(atomic_output_path.temporary_path())
                 }
                 "y" => {
                     field
                         .slice_across_y(&interpolator, coord, resampled_coord_locations)
-                        .save_as_pickle(output_file_path)
+                        .save_as_pickle(atomic_output_path.temporary_path())
                 }
                 "z" => {
                     field
                         .slice_across_z(&interpolator, coord, resampled_coord_locations)
-                        .save_as_pickle(output_file_path)
+                        .save_as_pickle(atomic_output_path.temporary_path())
                 }
                 invalid => exit_with_error!("Error: Invalid axis: {}", invalid),
             },
@@ -218,10 +221,69 @@ pub fn run_slice_subcommand<G, R>(
         };
 
         exit_on_error!(
-            field
-                .regular_slice_across_axis(&interpolator, axis, coord, location)
-                .save_as_pickle(output_file_path),
+            match output_type {
+                OutputType::Pickle => field
+                    .regular_slice_across_axis(&interpolator, axis, coord, location)
+                    .save_as_pickle(atomic_output_path.temporary_path()),
+            },
             "Error: Could not save output data: {}"
         );
+    }
+
+    exit_on_error!(
+        atomic_output_path.perform_replace(),
+        "Error: Could not move temporary output file to target path: {}"
+    );
+}
+
+#[derive(Copy, Clone, Debug)]
+enum OutputType {
+    Pickle,
+}
+
+impl OutputType {
+    fn from_path<P: AsRef<Path>>(file_path: P) -> Self {
+        Self::from_extension(
+            file_path
+                .as_ref()
+                .extension()
+                .unwrap_or_else(|| {
+                    exit_with_error!(
+                        "Error: Missing extension for output file\n\
+                         Valid extensions are: {}",
+                        Self::valid_extensions_string()
+                    )
+                })
+                .to_string_lossy()
+                .as_ref(),
+        )
+    }
+
+    fn from_extension(extension: &str) -> Self {
+        match extension {
+            "pickle" => Self::Pickle,
+            invalid => exit_with_error!(
+                "Error: Invalid extension {} for output file\n\
+                 Valid extensions are: {}",
+                invalid,
+                Self::valid_extensions_string()
+            ),
+        }
+    }
+
+    fn valid_extensions_string() -> String {
+        "pickle".to_string()
+    }
+}
+
+impl fmt::Display for OutputType {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            match self {
+                Self::Pickle => "pickle",
+            }
+        )
     }
 }
