f7e6cf39cfaf133e860675f22ce1fe4d5d403f50
Lars Frogner <lars_frogner@hotmail.com>
2020-11-25 12:38:17 +0100

    Add more derived quantities

--- src/field/quantities.rs
+++ src/field/quantities.rs
@@ -1,13 +1,15 @@
 //! Functions for computing various derived physical quantities.
 
 use crate::{
-    field::{self, ScalarField3},
+    field::{self, ResampledCoordLocation, ResamplingMethod, ScalarField3},
     geometry::In3D,
     grid::{CoordLocation, Grid3},
+    interpolation::poly_fit::{PolyFitInterpolator3, PolyFitInterpolatorConfig},
     io::{
         snapshot::{fdt, SnapshotReader3},
         Verbose,
     },
+    units::solar::{U_B, U_U},
 };
 use lazy_static::lazy_static;
 use ndarray::prelude::*;
@@ -16,6 +18,78 @@ use std::{collections::HashMap, io};
 
 lazy_static! {
     static ref DERIVED_QUANTITIES: HashMap<&'static str, (&'static str, Vec<&'static str>)> = vec![
+        (
+            "bx_cgs",
+            (
+                "Magnetic field in x-direction (original locations)\n\
+                 [G]",
+                vec!["bx"]
+            )
+        ),
+        (
+            "by_cgs",
+            (
+                "Magnetic field in y-direction (original locations)\n\
+                 [G]",
+                vec!["by"]
+            )
+        ),
+        (
+            "bz_cgs",
+            (
+                "Magnetic field in z-direction (original locations)\n\
+                 [G]",
+                vec!["bz"]
+            )
+        ),
+        (
+            "ux_cgs",
+            (
+                "Velocity in x-direction (cell centered)\n\
+                 [cm/s]",
+                vec!["r", "px"]
+            )
+        ),
+        (
+            "uy_cgs",
+            (
+                "Velocity in y-direction (cell centered)\n\
+                 [cm/s]",
+                vec!["r", "py"]
+            )
+        ),
+        (
+            "uz_cgs",
+            (
+                "Velocity in z-direction (cell centered)\n\
+                 [cm/s]",
+                vec!["r", "pz"]
+            )
+        ),
+        (
+            "ux",
+            (
+                "Velocity in x-direction (cell centered)\n\
+                 [10 km/s]",
+                vec!["r", "px"]
+            )
+        ),
+        (
+            "uy",
+            (
+                "Velocity in y-direction (cell centered)\n\
+                 [10 km/s]",
+                vec!["r", "py"]
+            )
+        ),
+        (
+            "uz",
+            (
+                "Velocity in z-direction (cell centered)\n\
+                 [10 km/s]",
+                vec!["r", "pz"]
+            )
+        ),
         (
             "ubeam",
             (
@@ -105,6 +179,15 @@ where
         println!("Computing {}", name);
     }
     let (values, locations) = match name {
+        "bx_cgs" => compute_bx_cgs_values(reader),
+        "by_cgs" => compute_by_cgs_values(reader),
+        "bz_cgs" => compute_bz_cgs_values(reader),
+        "ux_cgs" => compute_ux_cgs_values(reader),
+        "uy_cgs" => compute_uy_cgs_values(reader),
+        "uz_cgs" => compute_uz_cgs_values(reader),
+        "ux" => compute_ux_values(reader),
+        "uy" => compute_uy_values(reader),
+        "uz" => compute_uz_values(reader),
         "ubeam" => compute_ubeam_values(reader),
         "log10_pos_qbeam" => compute_log10_pos_qbeam_values(reader),
         "log10_neg_qbeam" => compute_log10_neg_qbeam_values(reader),
@@ -119,6 +202,237 @@ where
         locations,
         values,
     ))
+}
+
+fn compute_bx_cgs_values<G, R>(reader: &R) -> io::Result<(Array3<fdt>, In3D<CoordLocation>)>
+where
+    G: Grid3<fdt>,
+    R: SnapshotReader3<G>,
+{
+    let field = reader.read_scalar_field("bx")?;
+    let coord_locations = field.locations().clone();
+    let mut values = field.into_values();
+    let values_buffer = values.as_slice_memory_order_mut().unwrap();
+
+    values_buffer.par_iter_mut().for_each(|value| {
+        *value *= *U_B as fdt;
+    });
+
+    Ok((values, coord_locations))
+}
+
+fn compute_by_cgs_values<G, R>(reader: &R) -> io::Result<(Array3<fdt>, In3D<CoordLocation>)>
+where
+    G: Grid3<fdt>,
+    R: SnapshotReader3<G>,
+{
+    let field = reader.read_scalar_field("by")?;
+    let coord_locations = field.locations().clone();
+    let mut values = field.into_values();
+    let values_buffer = values.as_slice_memory_order_mut().unwrap();
+
+    values_buffer.par_iter_mut().for_each(|value| {
+        *value *= *U_B as fdt;
+    });
+
+    Ok((values, coord_locations))
+}
+
+fn compute_bz_cgs_values<G, R>(reader: &R) -> io::Result<(Array3<fdt>, In3D<CoordLocation>)>
+where
+    G: Grid3<fdt>,
+    R: SnapshotReader3<G>,
+{
+    let field = reader.read_scalar_field("bz")?;
+    let coord_locations = field.locations().clone();
+    let mut values = field.into_values();
+    let values_buffer = values.as_slice_memory_order_mut().unwrap();
+
+    values_buffer.par_iter_mut().for_each(|value| {
+        *value *= *U_B as fdt;
+    });
+
+    Ok((values, coord_locations))
+}
+
+fn compute_ux_cgs_values<G, R>(reader: &R) -> io::Result<(Array3<fdt>, In3D<CoordLocation>)>
+where
+    G: Grid3<fdt>,
+    R: SnapshotReader3<G>,
+{
+    let interpolator = PolyFitInterpolator3::new(PolyFitInterpolatorConfig::default());
+    let px_field = reader.read_scalar_field("px")?;
+    let mut values = px_field
+        .resampled_to_grid(
+            reader.arc_with_grid(),
+            In3D::same(ResampledCoordLocation::center()),
+            &interpolator,
+            ResamplingMethod::DirectSampling,
+        )
+        .into_values();
+    let values_buffer = values.as_slice_memory_order_mut().unwrap();
+
+    let r_field = reader.read_scalar_field("r")?;
+    let r_buffer = r_field.values().as_slice_memory_order().unwrap();
+
+    values_buffer
+        .par_iter_mut()
+        .zip(r_buffer)
+        .for_each(|(value, &r)| {
+            *value *= (U_U as fdt) / r;
+        });
+
+    Ok((values, In3D::same(CoordLocation::Center)))
+}
+
+fn compute_uy_cgs_values<G, R>(reader: &R) -> io::Result<(Array3<fdt>, In3D<CoordLocation>)>
+where
+    G: Grid3<fdt>,
+    R: SnapshotReader3<G>,
+{
+    let interpolator = PolyFitInterpolator3::new(PolyFitInterpolatorConfig::default());
+    let py_field = reader.read_scalar_field("py")?;
+    let mut values = py_field
+        .resampled_to_grid(
+            reader.arc_with_grid(),
+            In3D::same(ResampledCoordLocation::center()),
+            &interpolator,
+            ResamplingMethod::DirectSampling,
+        )
+        .into_values();
+    let values_buffer = values.as_slice_memory_order_mut().unwrap();
+
+    let r_field = reader.read_scalar_field("r")?;
+    let r_buffer = r_field.values().as_slice_memory_order().unwrap();
+
+    values_buffer
+        .par_iter_mut()
+        .zip(r_buffer)
+        .for_each(|(value, &r)| {
+            *value *= (U_U as fdt) / r;
+        });
+
+    Ok((values, In3D::same(CoordLocation::Center)))
+}
+
+fn compute_uz_cgs_values<G, R>(reader: &R) -> io::Result<(Array3<fdt>, In3D<CoordLocation>)>
+where
+    G: Grid3<fdt>,
+    R: SnapshotReader3<G>,
+{
+    let interpolator = PolyFitInterpolator3::new(PolyFitInterpolatorConfig::default());
+    let pz_field = reader.read_scalar_field("pz")?;
+    let mut values = pz_field
+        .resampled_to_grid(
+            reader.arc_with_grid(),
+            In3D::same(ResampledCoordLocation::center()),
+            &interpolator,
+            ResamplingMethod::DirectSampling,
+        )
+        .into_values();
+    let values_buffer = values.as_slice_memory_order_mut().unwrap();
+
+    let r_field = reader.read_scalar_field("r")?;
+    let r_buffer = r_field.values().as_slice_memory_order().unwrap();
+
+    values_buffer
+        .par_iter_mut()
+        .zip(r_buffer)
+        .for_each(|(value, &r)| {
+            *value *= (U_U as fdt) / r;
+        });
+
+    Ok((values, In3D::same(CoordLocation::Center)))
+}
+
+fn compute_ux_values<G, R>(reader: &R) -> io::Result<(Array3<fdt>, In3D<CoordLocation>)>
+where
+    G: Grid3<fdt>,
+    R: SnapshotReader3<G>,
+{
+    let interpolator = PolyFitInterpolator3::new(PolyFitInterpolatorConfig::default());
+    let px_field = reader.read_scalar_field("px")?;
+    let mut values = px_field
+        .resampled_to_grid(
+            reader.arc_with_grid(),
+            In3D::same(ResampledCoordLocation::center()),
+            &interpolator,
+            ResamplingMethod::DirectSampling,
+        )
+        .into_values();
+    let values_buffer = values.as_slice_memory_order_mut().unwrap();
+
+    let r_field = reader.read_scalar_field("r")?;
+    let r_buffer = r_field.values().as_slice_memory_order().unwrap();
+
+    values_buffer
+        .par_iter_mut()
+        .zip(r_buffer)
+        .for_each(|(value, &r)| {
+            *value /= r;
+        });
+
+    Ok((values, In3D::same(CoordLocation::Center)))
+}
+
+fn compute_uy_values<G, R>(reader: &R) -> io::Result<(Array3<fdt>, In3D<CoordLocation>)>
+where
+    G: Grid3<fdt>,
+    R: SnapshotReader3<G>,
+{
+    let interpolator = PolyFitInterpolator3::new(PolyFitInterpolatorConfig::default());
+    let py_field = reader.read_scalar_field("py")?;
+    let mut values = py_field
+        .resampled_to_grid(
+            reader.arc_with_grid(),
+            In3D::same(ResampledCoordLocation::center()),
+            &interpolator,
+            ResamplingMethod::DirectSampling,
+        )
+        .into_values();
+    let values_buffer = values.as_slice_memory_order_mut().unwrap();
+
+    let r_field = reader.read_scalar_field("r")?;
+    let r_buffer = r_field.values().as_slice_memory_order().unwrap();
+
+    values_buffer
+        .par_iter_mut()
+        .zip(r_buffer)
+        .for_each(|(value, &r)| {
+            *value /= r;
+        });
+
+    Ok((values, In3D::same(CoordLocation::Center)))
+}
+
+fn compute_uz_values<G, R>(reader: &R) -> io::Result<(Array3<fdt>, In3D<CoordLocation>)>
+where
+    G: Grid3<fdt>,
+    R: SnapshotReader3<G>,
+{
+    let interpolator = PolyFitInterpolator3::new(PolyFitInterpolatorConfig::default());
+    let pz_field = reader.read_scalar_field("pz")?;
+    let mut values = pz_field
+        .resampled_to_grid(
+            reader.arc_with_grid(),
+            In3D::same(ResampledCoordLocation::center()),
+            &interpolator,
+            ResamplingMethod::DirectSampling,
+        )
+        .into_values();
+    let values_buffer = values.as_slice_memory_order_mut().unwrap();
+
+    let r_field = reader.read_scalar_field("r")?;
+    let r_buffer = r_field.values().as_slice_memory_order().unwrap();
+
+    values_buffer
+        .par_iter_mut()
+        .zip(r_buffer)
+        .for_each(|(value, &r)| {
+            *value /= r;
+        });
+
+    Ok((values, In3D::same(CoordLocation::Center)))
 }
 
 fn compute_ubeam_values<G, R>(reader: &R) -> io::Result<(Array3<fdt>, In3D<CoordLocation>)>
