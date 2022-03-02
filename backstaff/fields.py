2e1511039e5cc98b664f923b786f084c26420b7f
Lars Frogner <lars.frogner@astro.uio.no>
2022-03-02 10:10:06 +0100

    Add methods for 3D scalar field class and fix minor issues

--- backstaff/fields.py
+++ backstaff/fields.py
@@ -15,12 +15,20 @@ class Coords3:
         self.x = np.asfarray(x_coords)
         self.y = np.asfarray(y_coords)
         self.z = np.asfarray(z_coords)
+        self.coords = (self.x, self.y, self.z)
 
     def get_shape(self):
         return self.x.size, self.y.size, self.z.size
 
+    def __getitem__(self, idx):
+        return self.coords[idx]
+
 
 class Coords2:
+    @staticmethod
+    def indices(shape):
+        return Coords2(np.arange(shape[0]), np.arange(shape[1]))
+
     @staticmethod
     def from_bifrost_data(bifrost_data, omitted_axis):
         all_coords = [
@@ -65,13 +73,12 @@ class ScalarField3:
         k_slice = slice(*((None, ) if height_range is None else np.
                           searchsorted(z_coords, height_range)))
         values = value_processor(
-            *(bifrost_data.get_var(quantity)[:, :, ::-1][400, 400, k_slice]
+            *(bifrost_data.get_var(quantity)[:, :, ::-1][:, :, k_slice]
               for quantity in quantities))
         z_coords = z_coords[k_slice]
         if scale is not None:
             values *= scale
 
-        return ScalarField1(Coords1(z_coords), values)
         return ScalarField3(Coords3(bifrost_data.x, bifrost_data.y, z_coords),
                             values)
 
@@ -121,6 +128,65 @@ class ScalarField3:
 
     def get_values_flat(self):
         return np.ravel(self.values)
+
+    def get_coords(self, inverted_vertically=False):
+        return (self.get_horizontal_width_coords(),
+                self.get_horizontal_depth_coords(),
+                self.get_height_coords(inverted=inverted_vertically))
+
+    def get_horizontal_width_coords(self):
+        return self.coords.x
+
+    def get_horizontal_depth_coords(self):
+        return self.coords.y
+
+    def get_height_coords(self, inverted=False):
+        return -self.coords.z[::-1] if inverted else self.coords.z
+
+    def resampled_along_axis(self,
+                             axis,
+                             resampling_factor,
+                             index_range=None,
+                             kind='linear'):
+        if index_range is None:
+            index_range = (0, self.get_shape()[axis])
+        slices = [slice(None)]*3
+        slices[axis] = slice(*index_range)
+        slices = tuple(slices)
+
+        axis_size = index_range[1] - index_range[0]
+        new_axis_size = int(np.ceil(resampling_factor*axis_size))
+
+        new_coords = [self.coords[i][slice] for i, slice in enumerate(slices)]
+        new_coords[axis] = np.linspace(new_coords[axis][0],
+                                       new_coords[axis][-1], new_axis_size)
+
+        new_values = scipy.interpolate.interp1d(
+            self.coords[axis][slices[axis]],
+            self.values[slices],
+            axis=axis,
+            kind=kind,
+            copy=False,
+            assume_sorted=True)(new_coords[axis])
+
+        return ScalarField3(Coords3(*new_coords), new_values)
+
+    def resampled_to_coords_along_axis(self,
+                                       axis,
+                                       new_axis_coords,
+                                       kind='linear'):
+        new_coords = [self.coords[i] for i in range(self.values.ndim)]
+        new_coords[axis] = new_axis_coords
+
+        new_values = scipy.interpolate.interp1d(
+            self.coords[axis],
+            self.values,
+            axis=axis,
+            kind=kind,
+            copy=False,
+            assume_sorted=True)(new_axis_coords)
+
+        return ScalarField3(Coords3(*new_coords), new_values)
 
 
 class ScalarField1:
