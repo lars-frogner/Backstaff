1a34a92e497dd7d1e8d6dde7e7fc50e8df7c0644
Lars Frogner <lars_frogner@hotmail.com>
2022-04-12 17:41:37 +0200

    Implement command line interface for synthesizing spectral lines

--- src/cli/snapshot/synthesize.rs
+++ src/cli/snapshot/synthesize.rs
@@ -1,10 +1,17 @@
 //! Command line interface for computing synthesized quantities for a snapshot.
 
 use crate::{
-    field::{quantities::DerivedSnapshotProvider3, synthesis::EmissivityTables},
+    cli::{
+        interpolation::poly_fit::{
+            construct_poly_fit_interpolator_config_from_options,
+            create_poly_fit_interpolator_subcommand,
+        },
+        utils,
+    },
+    field::synthesis::EmissivitySnapshotProvider3,
     grid::Grid3,
+    interpolation::poly_fit::{PolyFitInterpolator2, PolyFitInterpolatorConfig},
     io::snapshot::{fdt, SnapshotProvider3},
-    io::Verbose,
 };
 use clap::{Arg, ArgMatches, Command};
 
@@ -18,67 +25,136 @@ pub fn create_synthesize_subcommand(parent_command_name: &'static str) -> Comman
         .about("Computes synthetic quantities for the snapshot")
         .long_about("Computes synthetic quantities for the snapshot.")
         .arg(
+            Arg::new("spectral-lines")
+                .short('L')
+                .long("spectral-lines")
+                .require_equals(true)
+                .use_value_delimiter(true)
+                .require_value_delimiter(true)
+                .value_name("LINES")
+                .help(
+                    "List of spectral lines to synthesize, in format <ion>_<wavelength in Å>\n\
+                     (e.g. si_4_1393.755) (comma-separated) [default: none]",
+                )
+                .takes_value(true)
+                .multiple_values(true),
+        )
+        .arg(
+            Arg::new("n-table-temperatures")
+                .short('n')
+                .long("n-table-temperatures")
+                .require_equals(true)
+                .value_name("NUMBER")
+                .allow_hyphen_values(false)
+                .help("Number of temperatures to use in emissivity tables")
+                .takes_value(true)
+                .default_value("100"),
+        )
+        .arg(
+            Arg::new("n-table-electron-densities")
+                .short('m')
+                .long("n-table-electron-densities")
+                .require_equals(true)
+                .value_name("NUMBER")
+                .allow_hyphen_values(false)
+                .help("Number of electron densities to use in emissivity tables")
+                .takes_value(true)
+                .default_value("100"),
+        )
+        .arg(
+            Arg::new("table-temperature-limits")
+                .short('t')
+                .long("table-temperature-limits")
+                .require_equals(true)
+                .use_value_delimiter(true)
+                .require_value_delimiter(true)
+                .allow_hyphen_values(true)
+                .value_names(&["LOWER", "UPPER"])
+                .help(
+                    "Limits for temperature to use in emissivity tables\n\
+                     (in log₁₀ of K)",
+                )
+                .takes_value(true)
+                .default_value("3,7"),
+        )
+        .arg(
+            Arg::new("table-electron-density-limits")
+                .short('e')
+                .long("table-electron-density-limits")
+                .require_equals(true)
+                .use_value_delimiter(true)
+                .require_value_delimiter(true)
+                .allow_hyphen_values(true)
+                .value_names(&["LOWER", "UPPER"])
+                .help(
+                    "Limits for electron density to use in emissivity tables\n\
+                     (in log₁₀ of cm⁻³)",
+                )
+                .takes_value(true)
+                .default_value("8,13"),
+        )
+        .arg(
             Arg::new("verbose")
                 .short('v')
                 .long("verbose")
                 .help("Print status messages related to computation of synthetic quantities"),
         )
+        .subcommand(create_poly_fit_interpolator_subcommand(command_name))
 }
 
 #[cfg(feature = "synthesis")]
-pub fn create_synthesize_provider<G, P>(arguments: &ArgMatches, provider: P) -> P
+pub fn create_synthesize_provider<G, P>(
+    arguments: &ArgMatches,
+    provider: P,
+) -> EmissivitySnapshotProvider3<G, P, PolyFitInterpolator2>
 where
     G: Grid3<fdt>,
     P: SnapshotProvider3<G>,
 {
-    use rand::Rng;
+    let line_names = arguments
+        .values_of("spectral-lines")
+        .map(|values| values.map(String::from).collect::<Vec<_>>())
+        .unwrap_or(Vec::new());
 
-    use crate::interpolation::poly_fit::{PolyFitInterpolator2, PolyFitInterpolatorConfig};
+    if !line_names.is_empty() {
+        for variable_name in ["tg", "nel"] {
+            exit_on_false!(
+                provider.has_variable(variable_name),
+                "Error: Missing variable {} required for computing emissivities",
+                variable_name
+            );
+        }
+    }
 
-    let ion_lines = [("si_4".to_owned(), vec![1393.755])].into();
-    let n_temperature_points = 100;
-    let n_electron_density_points = 100;
-    let log_temperature_limits = (3.0, 7.0);
-    let log_electron_density_limits = (8.0, 13.0);
-    let verbose = Verbose::Yes;
+    let n_temperature_points =
+        utils::get_value_from_required_parseable_argument(arguments, "n-table-temperatures");
 
-    let interpolator = PolyFitInterpolator2::new(PolyFitInterpolatorConfig {
-        order: 1,
-        ..PolyFitInterpolatorConfig::default()
-    });
+    let n_electron_density_points =
+        utils::get_value_from_required_parseable_argument(arguments, "n-table-electron-densities");
 
-    let emissivity_tables = EmissivityTables::<fdt>::new(
-        &ion_lines,
+    let log_temperature_limits = utils::parse_limits(arguments, "table-temperature-limits", false);
+    let log_electron_density_limits =
+        utils::parse_limits(arguments, "table-electron-density-limits", false);
+
+    let verbose = arguments.is_present("verbose").into();
+
+    let interpolator_config = if let Some(interpolator_arguments) =
+        arguments.subcommand_matches("poly_fit_interpolator")
+    {
+        construct_poly_fit_interpolator_config_from_options(interpolator_arguments)
+    } else {
+        PolyFitInterpolatorConfig::default()
+    };
+    let interpolator = PolyFitInterpolator2::new(interpolator_config);
+
+    EmissivitySnapshotProvider3::new(
+        provider,
+        interpolator,
+        line_names,
         n_temperature_points,
         n_electron_density_points,
         log_temperature_limits,
         log_electron_density_limits,
         verbose,
-    );
-
-    let mut rng = rand::thread_rng();
-
-    let ten: fdt = 10.0;
-    let min_tg = ten.powf(log_temperature_limits.0);
-    let max_tg = ten.powf(log_temperature_limits.1);
-    let min_nel = ten.powf(log_electron_density_limits.0);
-    let max_nel = ten.powf(log_electron_density_limits.1);
-
-    const N_POINTS: usize = 768 * 768 * 768;
-    let temperatures: Vec<fdt> = (0..N_POINTS)
-        .map(|_| min_tg + rng.gen::<fdt>() * (max_tg - min_tg))
-        .collect();
-    let electron_densities: Vec<fdt> = (0..N_POINTS)
-        .map(|_| min_nel + rng.gen::<fdt>() * (max_nel - min_nel))
-        .collect();
-
-    let result = emissivity_tables.evaluate(
-        &interpolator,
-        "si_4_1393.755",
-        &temperatures,
-        &electron_densities,
-    );
-    println!("{}", result[0]);
-
-    provider
+    )
 }
--- src/field/synthesis.rs
+++ src/field/synthesis.rs
@@ -1,41 +1,230 @@
 //! Synthesis of spectral lines.
 
-use std::{
-    collections::{hash_map::Keys, HashMap},
-    process,
-    sync::Arc,
-    time::Instant,
-};
-
-use ndarray::{Array1, Array2, Array3};
-use numpy::{Element, PyArray1, PyArray2, PyArray3};
-use pyo3::{types::IntoPyDict, IntoPy, Py, PyAny, PyResult, Python};
-use rayon::prelude::*;
-
+use super::{ScalarField3, ScalarFieldProvider3};
 use crate::{
     field::ScalarField2,
-    geometry::{Coords2, In2D, Point2},
-    grid::{regular::RegularGrid2, CoordLocation, Grid2},
+    geometry::{Coords2, In2D, In3D, Point2},
+    grid::{regular::RegularGrid2, CoordLocation, Grid2, Grid3},
     interpolation::Interpolator2,
-    io::Verbose,
+    io::{
+        snapshot::{fdt, SnapshotProvider3},
+        Endianness, Verbose,
+    },
     num::BFloat,
 };
+use lazy_static::lazy_static;
+use ndarray::{Array2, Array3, ShapeBuilder};
+use numpy::{Element, PyArray1, PyArray2};
+use pyo3::{
+    exceptions::PyValueError, types::IntoPyDict, IntoPy, Py, PyAny, PyErr, PyResult, Python,
+};
+use rayon::prelude::*;
+use regex::Regex;
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    io,
+    marker::PhantomData,
+    mem::MaybeUninit,
+    process,
+    str::FromStr,
+    sync::Arc,
+};
+
+fn run_python_with_result<C, R>(command: C) -> R
+where
+    C: FnOnce(Python) -> PyResult<R>,
+{
+    Python::with_gil(|py| match command(py) {
+        Ok(result) => result,
+        Err(err) => {
+            err.print(py);
+            process::exit(1)
+        }
+    })
+}
+
+macro_rules! with_py_error {
+    ($expr:expr, $err_type:ty) => {
+        $expr.map_err(|err| PyErr::new::<$err_type, _>(format!("{}", err)))
+    };
+}
 
 type EmissivityTableMap<F> = HashMap<String, ScalarField2<F, RegularGrid2<F>>>;
 
-pub struct EmissivityTables<F: BFloat> {
-    emissivity_tables: EmissivityTableMap<F>,
-    table_grid: Arc<RegularGrid2<F>>,
+/// Computer of emissivities from Bifrost 3D simulation snapshots.
+#[derive(Clone, Debug)]
+pub struct EmissivitySnapshotProvider3<G, P, I> {
+    provider: P,
+    interpolator: I,
+    emissivity_quantity_names: Vec<String>,
+    auxiliary_variable_names: Vec<String>,
+    all_variable_names: Vec<String>,
+    emissivity_tables: EmissivityTables<fdt>,
     verbose: Verbose,
+    phantom: PhantomData<G>,
 }
 
-impl<'a, F> EmissivityTables<F>
+impl<G, P, I> EmissivitySnapshotProvider3<G, P, I>
 where
-    F: BFloat + Element + IntoPy<Py<PyAny>> + 'a,
-    &'a HashMap<String, Vec<F>>: IntoPyDict,
+    G: Grid3<fdt>,
+    P: SnapshotProvider3<G>,
+    I: Interpolator2,
 {
+    /// Creates a computer of emissivities.
     pub fn new(
-        ion_lines: &'a HashMap<String, Vec<F>>,
+        provider: P,
+        interpolator: I,
+        line_names: Vec<String>,
+        n_temperature_points: usize,
+        n_electron_density_points: usize,
+        log_temperature_limits: (fdt, fdt),
+        log_electron_density_limits: (fdt, fdt),
+        verbose: Verbose,
+    ) -> Self {
+        let emissivity_tables = EmissivityTables::new(
+            &line_names,
+            n_temperature_points,
+            n_electron_density_points,
+            log_temperature_limits,
+            log_electron_density_limits,
+            verbose,
+        );
+
+        let emissivity_quantity_names = line_names;
+
+        let mut auxiliary_variable_names: Vec<_> = provider.auxiliary_variable_names().to_vec();
+        auxiliary_variable_names.append(&mut emissivity_quantity_names.clone());
+
+        let mut all_variable_names = provider.primary_variable_names().to_vec();
+        all_variable_names.append(&mut auxiliary_variable_names.clone());
+
+        Self {
+            provider,
+            interpolator,
+            emissivity_quantity_names,
+            auxiliary_variable_names,
+            all_variable_names,
+            emissivity_tables,
+            verbose: verbose,
+            phantom: PhantomData,
+        }
+    }
+
+    fn evaluate_emissivities(&mut self, line_name: &str) -> io::Result<ScalarField3<fdt, G>> {
+        if self.verbose.is_yes() {
+            println!("Synthesizing {}", line_name);
+        }
+
+        let temperatures = self.provider.provide_scalar_field("tg")?;
+        let electron_densities = self.provider.provide_scalar_field("nel")?;
+
+        let temperature_buffer = temperatures.values().as_slice_memory_order().unwrap();
+        let electron_density_buffer = electron_densities.values().as_slice_memory_order().unwrap();
+
+        let mut emissivities = Array3::uninit(temperatures.values().raw_dim().f());
+        let emissivity_buffer = emissivities.as_slice_memory_order_mut().unwrap();
+
+        self.emissivity_tables.evaluate(
+            emissivity_buffer,
+            &self.interpolator,
+            line_name,
+            temperature_buffer,
+            electron_density_buffer,
+        );
+
+        let emissivities = unsafe { emissivities.assume_init() };
+
+        Ok(ScalarField3::new(
+            line_name.to_string(),
+            temperatures.arc_with_grid(),
+            In3D::same(CoordLocation::Center),
+            emissivities,
+        ))
+    }
+}
+
+impl<G, P, I> ScalarFieldProvider3<fdt, G> for EmissivitySnapshotProvider3<G, P, I>
+where
+    G: Grid3<fdt>,
+    P: SnapshotProvider3<G>,
+    I: Interpolator2,
+{
+    fn grid(&self) -> &G {
+        self.provider.grid()
+    }
+
+    fn arc_with_grid(&self) -> Arc<G> {
+        self.provider.arc_with_grid()
+    }
+
+    fn produce_scalar_field<S: AsRef<str>>(
+        &mut self,
+        variable_name: S,
+    ) -> io::Result<ScalarField3<fdt, G>> {
+        let variable_name = variable_name.as_ref();
+        if self.provider.has_variable(variable_name) {
+            self.provider.produce_scalar_field(variable_name)
+        } else {
+            self.evaluate_emissivities(variable_name)
+        }
+    }
+}
+
+impl<G, P, I> SnapshotProvider3<G> for EmissivitySnapshotProvider3<G, P, I>
+where
+    G: Grid3<fdt>,
+    P: SnapshotProvider3<G>,
+    I: Interpolator2,
+{
+    type Parameters = P::Parameters;
+
+    fn parameters(&self) -> &Self::Parameters {
+        self.provider.parameters()
+    }
+
+    fn endianness(&self) -> Endianness {
+        self.provider.endianness()
+    }
+
+    fn primary_variable_names(&self) -> &[String] {
+        self.provider.primary_variable_names()
+    }
+
+    fn auxiliary_variable_names(&self) -> &[String] {
+        &self.auxiliary_variable_names
+    }
+
+    fn all_variable_names(&self) -> &[String] {
+        &self.all_variable_names
+    }
+
+    fn obtain_snap_name_and_num(&self) -> (String, Option<u32>) {
+        self.provider.obtain_snap_name_and_num()
+    }
+}
+
+lazy_static! {
+    static ref ION_LINE_REGEX: Regex =
+        Regex::new(r"^([a-zA-Z]+)_([0-9]+)_([0-9]+(:?\.[0-9]*)?)$").unwrap();
+}
+
+/// Holds tables of emissivity as function of temperature and electron density
+/// for optically thin spectral lines.
+#[derive(Clone, Debug)]
+pub struct EmissivityTables<F: BFloat> {
+    emissivity_tables: EmissivityTableMap<F>,
+}
+
+impl<F> EmissivityTables<F>
+where
+    F: BFloat + FromStr + Element + IntoPy<Py<PyAny>>,
+    HashMap<String, Vec<F>>: IntoPyDict,
+    <F as FromStr>::Err: std::fmt::Display,
+{
+    /// Computes emissivity tables with the given shape and bounds for the given spectral lines,
+    /// and stores them in a new `EmissivityTables` object.
+    pub fn new(
+        line_names: &[String],
         n_temperature_points: usize,
         n_electron_density_points: usize,
         log_temperature_limits: (F, F),
@@ -46,14 +235,17 @@ where
         assert!(n_electron_density_points > 1);
 
         let (log_table_temperatures, log_table_electron_densities, emissivity_tables) =
-            Self::compute_emissivity_tables(
-                ion_lines,
-                n_temperature_points,
-                n_electron_density_points,
-                log_temperature_limits,
-                log_electron_density_limits,
-                verbose,
-            );
+            run_python_with_result(|py| {
+                Self::compute_emissivity_tables_py(
+                    py,
+                    line_names,
+                    n_temperature_points,
+                    n_electron_density_points,
+                    log_temperature_limits,
+                    log_electron_density_limits,
+                    verbose,
+                )
+            });
 
         let half_log_temperature_interval =
             F::from_f32(0.5).unwrap() * (log_table_temperatures[1] - log_table_temperatures[0]);
@@ -92,20 +284,18 @@ where
             })
             .collect();
 
-        Self {
-            emissivity_tables,
-            table_grid,
-            verbose,
-        }
+        Self { emissivity_tables }
     }
 
+    /// Provides sampled emissivities for the given spectral line, temperatures and electron densities.
     pub fn evaluate<I: Interpolator2>(
         &self,
+        emissivity_buffer: &mut [MaybeUninit<F>],
         interpolator: &I,
         line_name: &str,
         temperatures: &[F],
         electron_densities: &[F],
-    ) -> Array1<F> {
+    ) {
         let n_samples = temperatures.len();
         assert_eq!(electron_densities.len(), n_samples);
 
@@ -115,16 +305,7 @@ where
             line_name
         );
 
-        if self.verbose.is_yes() {
-            println!("Looking up {} emissivities for {}", n_samples, line_name);
-        }
-
-        let mut emissivities = Array1::uninit(temperatures.len());
-        let emissivities_buffer = emissivities.as_slice_memory_order_mut().unwrap();
-
-        let start_instant = Instant::now();
-
-        emissivities_buffer
+        emissivity_buffer
             .par_iter_mut()
             .zip(temperatures)
             .zip(electron_densities)
@@ -136,50 +317,17 @@ where
                         .expect_inside(),
                 );
             });
-
-        println!("Elapsed time: {} s", start_instant.elapsed().as_secs_f64());
-
-        unsafe { emissivities.assume_init() }
-    }
-
-    fn compute_emissivity_tables(
-        ion_lines: &'a HashMap<String, Vec<F>>,
-        n_temperature_points: usize,
-        n_electron_density_points: usize,
-        log_temperature_limits: (F, F),
-        log_electron_density_limits: (F, F),
-        verbose: Verbose,
-    ) -> (Vec<F>, Vec<F>, HashMap<String, Array2<F>>) {
-        Python::with_gil(|py| {
-            match Self::compute_emissivity_tables_py(
-                py,
-                ion_lines,
-                n_temperature_points,
-                n_electron_density_points,
-                log_temperature_limits,
-                log_electron_density_limits,
-                verbose,
-            ) {
-                Ok(result) => result,
-                Err(err) => {
-                    err.print(py);
-                    process::exit(1)
-                }
-            }
-        })
     }
 
     fn compute_emissivity_tables_py(
         py: Python,
-        ion_lines: &'a HashMap<String, Vec<F>>,
+        line_names: &[String],
         n_temperature_points: usize,
         n_electron_density_points: usize,
         log_temperature_limits: (F, F),
         log_electron_density_limits: (F, F),
         verbose: Verbose,
     ) -> PyResult<(Vec<F>, Vec<F>, HashMap<String, Array2<F>>)> {
-        let ion_lines_py = ion_lines.into_py_dict(py);
-
         let kwargs = [
             ("dtype", F::get_dtype(py).into_py(py)),
             ("n_temperature_points", n_temperature_points.into_py(py)),
@@ -197,9 +345,21 @@ where
         .into_py_dict(py);
 
         let syn = py.import("backstaff.chianti_synthesis")?;
-        let result = syn
-            .getattr("compute_emissivity_tables")?
-            .call((ion_lines_py,), Some(kwargs))?;
+
+        let valid_elements: Vec<String> = syn.getattr("get_valid_elements")?.call0()?.extract()?;
+
+        let (ion_line_name_map, ion_line_wavelength_map) = with_py_error!(
+            Self::line_names_to_ion_maps(line_names, &valid_elements),
+            PyValueError
+        )?;
+
+        let result = syn.getattr("compute_emissivity_tables")?.call(
+            (
+                ion_line_name_map.into_py_dict(py),
+                ion_line_wavelength_map.into_py_dict(py),
+            ),
+            Some(kwargs),
+        )?;
 
         let (log_table_temperatures, log_table_electron_densities, emissivity_tables): (
             &PyArray1<F>,
@@ -223,5 +383,110 @@ where
                 .expect("Array not contiguous"),
             emissivity_tables,
         ))
+    }
+
+    fn line_names_to_ion_maps(
+        line_names: &[String],
+        valid_elements: &[String],
+    ) -> io::Result<(HashMap<String, Vec<String>>, HashMap<String, Vec<F>>)> {
+        let mut ion_line_name_map: HashMap<String, Vec<String>> = HashMap::new();
+        let mut ion_line_wavelength_map: HashMap<String, Vec<F>> = HashMap::new();
+
+        for line_name in line_names {
+            let line_name = line_name.to_string();
+            if let Some(groups) = ION_LINE_REGEX.captures(line_name.as_str()) {
+                let element_name = groups
+                    .get(1)
+                    .expect("Missing capture group")
+                    .as_str()
+                    .to_lowercase();
+
+                let z =
+                    if let Some(pos) = valid_elements
+                        .iter()
+                        .position(|valid_element_name| valid_element_name == &element_name)
+                    {
+                        pos + 1
+                    } else {
+                        return Err(io::Error::new(
+                            io::ErrorKind::InvalidInput,
+                            format!(
+                            "Invalid element {} for spectral line {}, supported elements are [{}]",
+                            &element_name, line_name, valid_elements.join(", ")
+                        ),
+                        ));
+                    };
+
+                let ionization_stage: usize = match groups
+                    .get(2)
+                    .expect("Missing capture group")
+                    .as_str()
+                    .parse()
+                {
+                    Ok(ionization_stage) => ionization_stage,
+                    Err(err) => {
+                        return Err(io::Error::new(
+                            io::ErrorKind::InvalidInput,
+                            format!(
+                                "Invalid ionization stage for spectral line {}: {}",
+                                line_name, err
+                            ),
+                        ))
+                    }
+                };
+
+                if ionization_stage > z {
+                    return Err(io::Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!(
+                            "Ionization stage {} for element {} exceeds maximum of {}",
+                            ionization_stage, &element_name, z
+                        ),
+                    ));
+                }
+
+                let ion_name = format!("{}_{}", element_name, ionization_stage);
+
+                let wavelength: F = match groups
+                    .get(3)
+                    .expect("Missing capture group")
+                    .as_str()
+                    .parse()
+                {
+                    Ok(wavelength) => wavelength,
+                    Err(err) => {
+                        return Err(io::Error::new(
+                            io::ErrorKind::InvalidInput,
+                            format!(
+                                "Invalid wavelength for spectral line {}: {}",
+                                line_name, err
+                            ),
+                        ))
+                    }
+                };
+                match ion_line_name_map.entry(ion_name.clone()) {
+                    Entry::Occupied(mut entry) => {
+                        entry.get_mut().push(line_name);
+                    }
+                    Entry::Vacant(entry) => {
+                        entry.insert(vec![line_name]);
+                    }
+                }
+                match ion_line_wavelength_map.entry(ion_name) {
+                    Entry::Occupied(mut entry) => {
+                        entry.get_mut().push(wavelength);
+                    }
+                    Entry::Vacant(entry) => {
+                        entry.insert(vec![wavelength]);
+                    }
+                }
+            } else {
+                return Err(io::Error::new(
+                        io::ErrorKind::InvalidInput,
+                        format!("Invalid format for spectral line {}, must be <element>_<ionization stage>_<wavelength>, e.g. si_4_1393.755", line_name),
+                    ));
+            }
+        }
+        Ok((ion_line_name_map, ion_line_wavelength_map))
     }
 }
