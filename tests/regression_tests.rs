ac76512bfa43d4d0add27eae32c19456f0c02b44
Lars Frogner <lars_frogner@hotmail.com>
2022-06-10 10:55:52 +0200

    Add test

--- tests/regression_tests.rs
+++ tests/regression_tests.rs
@@ -17,5 +17,24 @@ fn regular_mesh_is_correct() {
         "--y-bounds=-1,2",
         "--z-bounds=1,1.5",
     ]);
-    test.assert_mesh_files_similar();
+    test.assert_mesh_files_equal();
+}
+
+#[test]
+fn hor_regular_mesh_is_correct() {
+    let test = RegressionTest::for_output_file("hor_regular.mesh");
+    run([
+        "create_mesh",
+        test.output_path(),
+        "--overwrite",
+        "horizontally_regular",
+        "--shape=3,4,25",
+        "--x-bounds=0,1",
+        "--y-bounds=-1,2",
+        "--z-bounds=-15,2.5",
+        "--boundary-dz-scales=80,20",
+        "--interior-z=-2.5,0",
+        "--interior-dz-scales=10,10",
+    ]);
+    test.assert_mesh_files_equal();
 }
