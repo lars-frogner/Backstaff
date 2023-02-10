2f6032e0670f5e14423eeb574379c0e75070f377
Lars Frogner <lars_frogner@hotmail.com>
2023-02-10 12:20:48 +0100

    Switch to third order Heun method for stepping

--- src/ebeam/propagation/fp_characteristics/transport.rs
+++ src/ebeam/propagation/fp_characteristics/transport.rs
@@ -209,7 +209,7 @@ impl Transporter {
         if initial_pitch_angle_cos <= 0.0 {
             TransportResult::Thermalized
         } else {
-            self.advance_quantities_with_second_order_heun(
+            self.advance_quantities_with_third_order_heun(
                 initial_energy,
                 initial_pitch_angle_cos,
                 initial_number_density,
@@ -432,6 +432,72 @@ impl Transporter {
         let number_density = initial_number_density
             + 0.5
                 * (number_density_col_depth_deriv_1 + number_density_col_depth_deriv_2)
+                * col_depth_increase;
+
+        if energy <= 0.0 || pitch_angle_cos <= 0.0 {
+            TransportResult::Thermalized
+        } else {
+            TransportResult::NewValues((energy, pitch_angle_cos, feb::max(0.0, number_density)))
+        }
+    }
+
+    fn advance_quantities_with_third_order_heun(
+        &self,
+        initial_energy: feb,
+        initial_pitch_angle_cos: feb,
+        initial_number_density: feb,
+        col_depth_increase: feb,
+    ) -> TransportResult {
+        let ColumnDepthDerivatives {
+            energy: energy_col_depth_deriv_1,
+            pitch_angle: pitch_angle_cos_col_depth_deriv_1,
+            number_density: number_density_col_depth_deriv_1,
+        } = self.compute_col_depth_derivs(
+            initial_energy,
+            initial_pitch_angle_cos,
+            initial_number_density,
+        );
+
+        let energy_1 = initial_energy + energy_col_depth_deriv_1 * col_depth_increase / 3.0;
+        let pitch_angle_cos_1 =
+            initial_pitch_angle_cos + pitch_angle_cos_col_depth_deriv_1 * col_depth_increase / 3.0;
+        let number_density_1 =
+            initial_number_density + number_density_col_depth_deriv_1 * col_depth_increase / 3.0;
+
+        if energy_1 <= 0.0 || pitch_angle_cos_1 <= 0.0 {
+            return TransportResult::Thermalized;
+        }
+
+        let ColumnDepthDerivatives {
+            energy: energy_col_depth_deriv_2,
+            pitch_angle: pitch_angle_cos_col_depth_deriv_2,
+            number_density: number_density_col_depth_deriv_2,
+        } = self.compute_col_depth_derivs(energy_1, pitch_angle_cos_1, number_density_1);
+
+        let energy_2 = initial_energy + energy_col_depth_deriv_2 * col_depth_increase * 2.0 / 3.0;
+        let pitch_angle_cos_2 = initial_pitch_angle_cos
+            + pitch_angle_cos_col_depth_deriv_2 * col_depth_increase * 2.0 / 3.0;
+        let number_density_2 = initial_number_density
+            + number_density_col_depth_deriv_2 * col_depth_increase * 2.0 / 3.0;
+
+        if energy_2 <= 0.0 || pitch_angle_cos_2 <= 0.0 {
+            return TransportResult::Thermalized;
+        }
+
+        let ColumnDepthDerivatives {
+            energy: energy_col_depth_deriv_3,
+            pitch_angle: pitch_angle_cos_col_depth_deriv_3,
+            number_density: number_density_col_depth_deriv_3,
+        } = self.compute_col_depth_derivs(energy_2, pitch_angle_cos_2, number_density_2);
+
+        let energy = initial_energy
+            + (0.25 * energy_col_depth_deriv_1 + 0.75 * energy_col_depth_deriv_3)
+                * col_depth_increase;
+        let pitch_angle_cos = initial_pitch_angle_cos
+            + (0.25 * pitch_angle_cos_col_depth_deriv_1 + 0.75 * pitch_angle_cos_col_depth_deriv_3)
+                * col_depth_increase;
+        let number_density = initial_number_density
+            + (0.25 * number_density_col_depth_deriv_1 + 0.75 * number_density_col_depth_deriv_3)
                 * col_depth_increase;
 
         if energy <= 0.0 || pitch_angle_cos <= 0.0 {
